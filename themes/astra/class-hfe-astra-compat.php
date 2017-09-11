2d45741e49d315572377ac034b2e58e00030c388
Nikhil Chavan <email@nikhilchavan.com>
2017-09-12 00:32:47 +0530

    Reduce duplicate markup for Astra theme Compatibilit

--- themes/astra/class-hfe-astra-compat.php
+++ themes/astra/class-hfe-astra-compat.php
@@ -38,12 +38,12 @@ class HFE_Astra_Compat {
 
 		if ( hfe_header_enabled() ) {
 			add_action( 'template_redirect', array( $this, 'astra_setup_header' ), 10 );
-			add_action( 'astra_header', array( $this, 'add_header_markup' ) );
+			add_action( 'astra_header', 'hfe_render_header' );
 		}
 
 		if ( hfe_footer_enabled() ) {
 			add_action( 'template_redirect', array( $this, 'astra_setup_footer' ), 10 );
-			add_action( 'astra_footer', array( $this, 'add_footer_markup' ) );
+			add_action( 'astra_footer', 'hfe_render_footer' );
 		}
 
 	}
@@ -56,37 +56,11 @@ class HFE_Astra_Compat {
 	}
 
 	/**
-	 * Display header markup.
-	 */
-	public function add_header_markup() {
-		?>
-			<header id="masthead" itemscope="itemscope" itemtype="http://schema.org/WPHeader">
-				<p class="main-title bhf-hidden" itemprop="headline"><a href="<?php echo bloginfo( 'url' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php Header_Footer_Elementor::get_header_content(); ?>
-			</header>
-
-		<?php
-	}
-
-	/**
 	 * Disable footer from the theme.
 	 */
 	public function astra_setup_footer() {
 		remove_action( 'astra_footer', 'astra_footer_markup' );
 	}
-
-	/**
-	 * Display footer markup.
-	 */
-	public function add_footer_markup() {
-
-		?>
-			<footer itemscope="itemscope" itemtype="http://schema.org/WPFooter">
-				<?php Header_Footer_Elementor::get_footer_content(); ?>
-			</footer>
-		<?php
-	}
-
 
 }
 
