e03d20b62268f5e16dd2bc8523ac7ffd4c1c665e
Nikhil Chavan <email@nikhilchavan.com>
2019-09-18 18:50:46 +0530

    Add upgrade routine to set target rules for active header, footer and before footer locations

--- inc/class-hfe-update.php
+++ inc/class-hfe-update.php
@@ -59,33 +59,88 @@ if ( ! class_exists( 'HFE_Update' ) ) {
 
 			$db_version = get_option( $this->db_option_key, false );
 
-			if ( version_compare( $db_version, '1.2.0', '<' ) ) {
+			if ( version_compare( $db_version, '1.2.0-beta.1', '<' ) ) {
 				$this->setup_default_terget_rules();
 			}
 
 			// flush rewrite rules on plugin update.
 			flush_rewrite_rules();
 
-			// $this->update_db_version();
+			$this->update_db_version();
 
 			do_action( 'hfe_update_after' );
 		}
 
+		/**
+		 * Set default target rules for header, footer, before footers being used before target rules were added to the plugin.
+		 *
+		 * @since x.x.x
+		 * @return void
+		 */
 		private function setup_default_terget_rules() {
 			$default_include_locations = [
 				'rule'     => [ 0 => 'basic-global' ],
 				'specific' => [],
 			];
 
-			$default_exclude_locations = [];
+			$header_id        = $this->get_legacy_template_id( 'type_before_footer' );
+			$footer_id        = $this->get_legacy_template_id( 'type_before_footer' );
+			$before_footer_id = $this->get_legacy_template_id( 'type_before_footer' );
 
-			// Header
-			// update_post_meta( get_hfe_header_id(), 'ehf_target_include_locations' );
-			// update_post_meta( get_hfe_header_id(), 'ehf_target_exclude_locations' );
+			// Header.
+			if ( ! empty( $header_id ) ) {
+				update_post_meta( $header_id, 'ehf_target_include_locations', $default_include_locations );
+			}
 
-			// Footer
-			// update_post_meta( get_hfe_footer_id(), 'ehf_target_include_locations' );
-			// update_post_meta( get_hfe_footer_id(), 'ehf_target_exclude_locations' );
+			// Footer.
+			if ( ! empty( $footer_id ) ) {
+				update_post_meta( $footer_id, 'ehf_target_include_locations', $default_include_locations );
+			}
+
+			// Before Footer.
+			if ( ! empty( $before_footer_id ) ) {
+				update_post_meta( $before_footer_id, 'ehf_target_include_locations', $default_include_locations );
+			}
+		}
+
+		/**
+		 * Get header or footer template id based on the meta query.
+		 *
+		 * @param  String $type Type of the template header/footer.
+		 *
+		 * @return Mixed  Returns the header or footer template id if found, else returns string ''.
+		 */
+		public function get_legacy_template_id( $type ) {
+			$args = [
+				'post_type'    => 'elementor-hf',
+				'meta_key'     => 'ehf_template_type',
+				'meta_value'   => $type,
+				'meta_type'    => 'post',
+				'meta_compare' => '>=',
+				'orderby'      => 'meta_value',
+				'order'        => 'ASC',
+				'meta_query'   => [
+					'relation' => 'OR',
+					[
+						'key'     => 'ehf_template_type',
+						'value'   => $type,
+						'compare' => '==',
+						'type'    => 'post',
+					],
+				],
+			];
+
+			$args     = apply_filters( 'hfe_get_template_id_args', $args );
+			$template = new WP_Query(
+				$args
+			);
+
+			if ( $template->have_posts() ) {
+				$posts = wp_list_pluck( $template->posts, 'ID' );
+				return $posts[0];
+			}
+
+			return '';
 		}
 
 		/**
