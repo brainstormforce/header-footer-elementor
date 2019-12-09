d726520ccbc517d4721dcc28fdf9b9cb20c2b272
Nikhil Chavan <email@nikhilchavan.com>
2019-12-09 11:45:45 +0530

    Add grunt version-bum command

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,12 +41,80 @@ module.exports = function( grunt ) {
 				}
 			}
 		},
+
+		bumpup: {
+			options: {
+				updateProps: {
+					pkg: 'package.json'
+				}
+			},
+			file: 'package.json'
+		},
+
+		replace: {
+			plugin_main: {
+				src: ['header-footer-elementor.php'],
+				overwrite: true,
+				replacements: [
+					{
+						from: /Version: \bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-[\da-z-A-Z-]+(?:\.[\da-z-A-Z-]+)*)?(?:\+[\da-z-A-Z-]+(?:\.[\da-z-A-Z-]+)*)?\b/g,
+						to: 'Version: <%= pkg.version %>'
+					}
+				]
+			},
+
+			plugin_const: {
+				src: ['header-footer-elementor.php'],
+				overwrite: true,
+				replacements: [
+					{
+						from: /HFE_VER', '.*?'/g,
+						to: 'HFE_VER\', \'<%= pkg.version %>\''
+					}
+				]
+			},
+
+			plugin_function_comment: {
+				src: [
+					'*.php',
+					'**/*.php',
+					'!node_modules/**',
+					'!php-tests/**',
+					'!bin/**',
+					'!admin/bsf-core/**'
+				],
+				overwrite: true,
+				replacements: [
+					{
+						from: 'x.x.x',
+						to: '<%=pkg.version %>'
+					}
+				]
+			}
+		}
+
 	} );
 
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
+	grunt.loadNpmTasks( 'grunt-bumpup' );
+	grunt.loadNpmTasks( 'grunt-text-replace' );
+	
 	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
 	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
+
+	// Bump Version - `grunt version-bump --ver=<version-number>`
+    grunt.registerTask('version-bump', function (ver) {
+
+        var newVersion = grunt.option('ver');
+
+        if (newVersion) {
+            newVersion = newVersion ? newVersion : 'patch';
+
+            grunt.task.run('bumpup:' + newVersion);
+            grunt.task.run('replace');
+        }
+    });
 
 	grunt.util.linefeed = '\n';
 
