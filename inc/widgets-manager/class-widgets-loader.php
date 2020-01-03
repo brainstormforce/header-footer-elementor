e86d9b1eb812018cbd13b5fff829eb6b56bc6385
Abhijit Nage <abhijitn@bsf.io>
2020-01-03 16:00:56 +0530

    Function for a page title

--- inc/widgets-manager/class-widgets-loader.php
+++ inc/widgets-manager/class-widgets-loader.php
@@ -13,131 +13,125 @@ namespace HFE\WidgetsManager;
 
 use Elementor\Plugin;
 
-defined('ABSPATH') or exit;
+defined( 'ABSPATH' ) or exit;
 
 /**
  * Set up Widgets Loader class
  */
-class Widgets_Loader
-{
+class Widgets_Loader {
 
 
-    /**
-     * Instance of Widgets_Loader.
-     *
-     * @since  1.2.0
-     * @var null
-     */
-    private static $_instance = null;
 
-    /**
-     * Get instance of Widgets_Loader
-     *
-     * @since  1.2.0
-     * @return Widgets_Loader
-     */
-    public static function instance()
-    {
-        if (! isset(self::$_instance)) {
-            self::$_instance = new self();
-        }
+	/**
+	 * Instance of Widgets_Loader.
+	 *
+	 * @since  1.2.0
+	 * @var null
+	 */
+	private static $_instance = null;
 
-        return self::$_instance;
-    }
+	/**
+	 * Get instance of Widgets_Loader
+	 *
+	 * @since  1.2.0
+	 * @return Widgets_Loader
+	 */
+	public static function instance() {
+		if ( ! isset( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
 
-    /**
-     * Setup actions and filters.
-     *
-     * @since  1.2.0
-     */
-    private function __construct()
-    {
+		return self::$_instance;
+	}
 
-        // Register category.
-        add_action('elementor/elements/categories_registered', [ $this, 'register_widget_category' ]);
+	/**
+	 * Setup actions and filters.
+	 *
+	 * @since  1.2.0
+	 */
+	private function __construct() {
 
-        // Register widgets.
-        add_action('elementor/widgets/widgets_registered', [ $this, 'register_widgets' ]);
+		// Register category.
+		add_action( 'elementor/elements/categories_registered', [ $this, 'register_widget_category' ] );
 
-        // Add svg support.
-        add_filter('upload_mimes', [ $this, 'hfe_svg_mime_types' ]);
-    }
+		// Register widgets.
+		add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ] );
+
+		// Add svg support.
+		add_filter( 'upload_mimes', [ $this, 'hfe_svg_mime_types' ] );
+	}
 
 
-    /**
-     * Include Widgets files
-     *
-     * Load widgets files
-     *
-     * @since 1.2.0
-     * @access public
-     */
-    public function include_widgets_files()
-    {
+	/**
+	 * Include Widgets files
+	 *
+	 * Load widgets files
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function include_widgets_files() {
 
-        require_once HFE_DIR . '/inc/widgets-manager/widgets/class-retina.php';
-        require_once HFE_DIR . '/inc/widgets-manager/widgets/class-copyright.php';
-        require_once HFE_DIR . '/inc/widgets-manager/widgets/class-copyright-shortcode.php';
-        require_once HFE_DIR . '/inc/widgets-manager/widgets/class-site-logo.php';
-        require_once HFE_DIR . '/inc/widgets-manager/widgets/class-page-title.php';
-    }
+		require_once HFE_DIR . '/inc/widgets-manager/widgets/class-retina.php';
+		require_once HFE_DIR . '/inc/widgets-manager/widgets/class-copyright.php';
+		require_once HFE_DIR . '/inc/widgets-manager/widgets/class-copyright-shortcode.php';
+		require_once HFE_DIR . '/inc/widgets-manager/widgets/class-site-logo.php';
+		require_once HFE_DIR . '/inc/widgets-manager/widgets/class-page-title.php';
+	}
 
-    /**
-     * Provide the SVG support for Retina Logo widget.
-     *
-     * @param array $mimes which return mime type.
-     *
-     * @since  1.2.0
-     * @return $mimes.
-     */
-    public function hfe_svg_mime_types($mimes)
-    {
+	/**
+	 * Provide the SVG support for Retina Logo widget.
+	 *
+	 * @param array $mimes which return mime type.
+	 *
+	 * @since  1.2.0
+	 * @return $mimes.
+	 */
+	public function hfe_svg_mime_types( $mimes ) {
 
-        // New allowed mime types.
-        $mimes['svg'] = 'image/svg+xml';
-        return $mimes;
-    }
+		// New allowed mime types.
+		$mimes['svg'] = 'image/svg+xml';
+		return $mimes;
+	}
 
-    /**
-     * Register Category
-     *
-     * @since 1.2.0
-     * @param object $this_cat class.
-     */
-    public function register_widget_category($this_cat)
-    {
-        $category = __('Header, Footer & Blocks', 'header-footer-elementor');
+	/**
+	 * Register Category
+	 *
+	 * @since 1.2.0
+	 * @param object $this_cat class.
+	 */
+	public function register_widget_category( $this_cat ) {
+		$category = __( 'Header, Footer & Blocks', 'header-footer-elementor' );
 
-        $this_cat->add_category(
-            'hfe-widgets',
-            [
-                'title' => $category,
-                'icon'  => 'eicon-font',
-            ]
-        );
+		$this_cat->add_category(
+			'hfe-widgets',
+			[
+				'title' => $category,
+				'icon'  => 'eicon-font',
+			]
+		);
 
-        return $this_cat;
-    }
+		return $this_cat;
+	}
 
-    /**
-     * Register Widgets
-     *
-     * Register new Elementor widgets.
-     *
-     * @since 1.2.0
-     * @access public
-     */
-    public function register_widgets()
-    {
+	/**
+	 * Register Widgets
+	 *
+	 * Register new Elementor widgets.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function register_widgets() {
 
-        // Its is now safe to include Widgets files.
-        $this->include_widgets_files();
-        // Register Widgets.
-        Plugin::instance()->widgets_manager->register_widget_type(new Widgets\Retina());
-        Plugin::instance()->widgets_manager->register_widget_type(new Widgets\Copyright());
-        Plugin::instance()->widgets_manager->register_widget_type(new Widgets\Site_Logo());
-        Plugin::instance()->widgets_manager->register_widget_type(new Widgets\Page_Title());
-    }
+		// Its is now safe to include Widgets files.
+		$this->include_widgets_files();
+		// Register Widgets.
+		Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Retina() );
+		Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Copyright() );
+		Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Site_Logo() );
+		Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Page_Title() );
+	}
 }
 
 /**
--- inc/widgets-manager/widgets/class-page-title.php
+++ inc/widgets-manager/widgets/class-page-title.php
@@ -13,8 +13,8 @@ use Elementor\Group_Control_Text_Shadow;
 use Elementor\Core\Schemes;
 use Elementor\Widget_Base;
 
-if (! defined('ABSPATH')) {
-    exit;   // Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;   // Exit if accessed directly.
 }
 
 /**
@@ -24,348 +24,352 @@ if (! defined('ABSPATH')) {
  *
  * @since 1.2.0
  */
-class Page_Title extends Widget_Base
-{
+class Page_Title extends Widget_Base {
 
 
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.2.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'heading';
+	}
 
-    /**
-     * Retrieve the widget name.
-     *
-     * @since 1.2.0
-     *
-     * @access public
-     *
-     * @return string Widget name.
-     */
-    public function get_name()
-    {
-        return 'heading';
-    }
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.2.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Page Title', 'header-footer-elementor' );
+	}
 
-    /**
-     * Retrieve the widget title.
-     *
-     * @since 1.2.0
-     *
-     * @access public
-     *
-     * @return string Widget title.
-     */
-    public function get_title()
-    {
-        return __('Page Title', 'header-footer-elementor');
-    }
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.2.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-archive-title';
+	}
 
-    /**
-     * Retrieve the widget icon.
-     *
-     * @since 1.2.0
-     *
-     * @access public
-     *
-     * @return string Widget icon.
-     */
-    public function get_icon()
-    {
-        return 'eicon-archive-title';
-    }
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.2.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'hfe-widgets' ];
+	}
 
-    /**
-     * Retrieve the list of categories the widget belongs to.
-     *
-     * Used to determine where to display the widget in the editor.
-     *
-     * Note that currently Elementor supports only one category.
-     * When multiple categories passed, Elementor uses the first one.
-     *
-     * @since 1.2.0
-     *
-     * @access public
-     *
-     * @return array Widget categories.
-     */
-    public function get_categories()
-    {
-        return [ 'hfe-widgets' ];
-    }
+	/**
+	 * Register Site Logo controls.
+	 *
+	 * @since 1.2.0
+	 * @access protected
+	 */
+	protected function _register_controls() {
+		$this->register_content_page_title_controls();
+	}
 
-    /**
-     * Register Site Logo controls.
-     *
-     * @since 1.2.0
-     * @access protected
-     */
-    protected function _register_controls()
-    {
-        $this->register_content_page_title_controls();
-    }
+	/**
+	 * Register heading widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function register_content_page_title_controls() {
+		$this->start_controls_section(
+			'section_title',
+			[
+				'label' => __( 'Title', 'header-footer-elementor' ),
+			]
+		);
 
-    /**
-     * Register heading widget controls.
-     *
-     * Adds different input fields to allow the user to change and customize the widget settings.
-     *
-     * @since 1.0.0
-     * @access protected
-     */
-    protected function register_content_page_title_controls()
-    {
-        $this->start_controls_section(
-            'section_title',
-            [
-                'label' => __('Title', 'header-footer-elementor'),
-            ]
-        );
+		$this->add_control(
+			'title',
+			[
+				'label'   => __( 'Title', 'header-footer-elementor' ),
+				'type'    => Controls_Manager::TEXTAREA,
+				'dynamic' => [
+					'active' => true,
+				],
+				// 'placeholder' => __('Enter your title', 'header-footer-elementor'),
+				// 'default' => __('Add Your Heading Text Here', 'header-footer-elementor'),
+				'default' => $this->page_title(),
+			]
+		);
 
-        $this->add_control(
-            'title',
-            [
-                'label'   => __('Title', 'header-footer-elementor'),
-                'type'    => Controls_Manager::TEXTAREA,
-                'dynamic' => [
-                    'active' => true,
-                ],
-                // 'placeholder' => __('Enter your title', 'header-footer-elementor'),
-                // 'default' => __('Add Your Heading Text Here', 'header-footer-elementor'),
-                'default' => get_the_title(),
-            ]
-        );
+		$this->add_control(
+			'link',
+			[
+				'label'     => __( 'Link', 'header-footer-elementor' ),
+				'type'      => Controls_Manager::URL,
+				'dynamic'   => [
+					'active' => true,
+				],
+				'default'   => [
+					'url' => '',
+				],
+				'separator' => 'before',
+			]
+		);
 
-        $this->add_control(
-            'link',
-            [
-                'label'     => __('Link', 'header-footer-elementor'),
-                'type'      => Controls_Manager::URL,
-                'dynamic'   => [
-                    'active' => true,
-                ],
-                'default'   => [
-                    'url' => '',
-                ],
-                'separator' => 'before',
-            ]
-        );
+		$this->add_control(
+			'size',
+			[
+				'label'   => __( 'Size', 'header-footer-elementor' ),
+				'type'    => Controls_Manager::SELECT,
+				'default' => 'default',
+				'options' => [
+					'default' => __( 'Default', 'header-footer-elementor' ),
+					'small'   => __( 'Small', 'header-footer-elementor' ),
+					'medium'  => __( 'Medium', 'header-footer-elementor' ),
+					'large'   => __( 'Large', 'header-footer-elementor' ),
+					'xl'      => __( 'XL', 'header-footer-elementor' ),
+					'xxl'     => __( 'XXL', 'header-footer-elementor' ),
+				],
+			]
+		);
 
-        $this->add_control(
-            'size',
-            [
-                'label'   => __('Size', 'header-footer-elementor'),
-                'type'    => Controls_Manager::SELECT,
-                'default' => 'default',
-                'options' => [
-                    'default' => __('Default', 'header-footer-elementor'),
-                    'small'   => __('Small', 'header-footer-elementor'),
-                    'medium'  => __('Medium', 'header-footer-elementor'),
-                    'large'   => __('Large', 'header-footer-elementor'),
-                    'xl'      => __('XL', 'header-footer-elementor'),
-                    'xxl'     => __('XXL', 'header-footer-elementor'),
-                ],
-            ]
-        );
+		$this->add_control(
+			'header_size',
+			[
+				'label'   => __( 'HTML Tag', 'header-footer-elementor' ),
+				'type'    => Controls_Manager::SELECT,
+				'options' => [
+					'h1'   => 'H1',
+					'h2'   => 'H2',
+					'h3'   => 'H3',
+					'h4'   => 'H4',
+					'h5'   => 'H5',
+					'h6'   => 'H6',
+					'div'  => 'div',
+					'span' => 'span',
+					'p'    => 'p',
+				],
+				'default' => 'h2',
+			]
+		);
 
-        $this->add_control(
-            'header_size',
-            [
-                'label'   => __('HTML Tag', 'header-footer-elementor'),
-                'type'    => Controls_Manager::SELECT,
-                'options' => [
-                    'h1'   => 'H1',
-                    'h2'   => 'H2',
-                    'h3'   => 'H3',
-                    'h4'   => 'H4',
-                    'h5'   => 'H5',
-                    'h6'   => 'H6',
-                    'div'  => 'div',
-                    'span' => 'span',
-                    'p'    => 'p',
-                ],
-                'default' => 'h2',
-            ]
-        );
+		$this->add_responsive_control(
+			'align',
+			[
+				'label'     => __( 'Alignment', 'header-footer-elementor' ),
+				'type'      => Controls_Manager::CHOOSE,
+				'options'   => [
+					'left'    => [
+						'title' => __( 'Left', 'header-footer-elementor' ),
+						'icon'  => 'eicon-text-align-left',
+					],
+					'center'  => [
+						'title' => __( 'Center', 'header-footer-elementor' ),
+						'icon'  => 'eicon-text-align-center',
+					],
+					'right'   => [
+						'title' => __( 'Right', 'header-footer-elementor' ),
+						'icon'  => 'eicon-text-align-right',
+					],
+					'justify' => [
+						'title' => __( 'Justified', 'header-footer-elementor' ),
+						'icon'  => 'eicon-text-align-justify',
+					],
+				],
+				'default'   => '',
+				'selectors' => [
+					'{{WRAPPER}}' => 'text-align: {{VALUE}};',
+				],
+			]
+		);
 
-        $this->add_responsive_control(
-            'align',
-            [
-                'label'     => __('Alignment', 'header-footer-elementor'),
-                'type'      => Controls_Manager::CHOOSE,
-                'options'   => [
-                    'left'    => [
-                        'title' => __('Left', 'header-footer-elementor'),
-                        'icon'  => 'eicon-text-align-left',
-                    ],
-                    'center'  => [
-                        'title' => __('Center', 'header-footer-elementor'),
-                        'icon'  => 'eicon-text-align-center',
-                    ],
-                    'right'   => [
-                        'title' => __('Right', 'header-footer-elementor'),
-                        'icon'  => 'eicon-text-align-right',
-                    ],
-                    'justify' => [
-                        'title' => __('Justified', 'header-footer-elementor'),
-                        'icon'  => 'eicon-text-align-justify',
-                    ],
-                ],
-                'default'   => '',
-                'selectors' => [
-                    '{{WRAPPER}}' => 'text-align: {{VALUE}};',
-                ],
-            ]
-        );
+		$this->add_control(
+			'view',
+			[
+				'label'   => __( 'View', 'header-footer-elementor' ),
+				'type'    => Controls_Manager::HIDDEN,
+				'default' => 'traditional',
+			]
+		);
 
-        $this->add_control(
-            'view',
-            [
-                'label'   => __('View', 'header-footer-elementor'),
-                'type'    => Controls_Manager::HIDDEN,
-                'default' => 'traditional',
-            ]
-        );
+		$this->end_controls_section();
 
-        $this->end_controls_section();
+		$this->start_controls_section(
+			'section_title_style',
+			[
+				'label' => __( 'Title', 'header-footer-elementor' ),
+				'tab'   => Controls_Manager::TAB_STYLE,
+			]
+		);
 
-        $this->start_controls_section(
-            'section_title_style',
-            [
-                'label' => __('Title', 'header-footer-elementor'),
-                'tab'   => Controls_Manager::TAB_STYLE,
-            ]
-        );
+		$this->add_control(
+			'title_color',
+			[
+				'label'     => __( 'Text Color', 'header-footer-elementor' ),
+				'type'      => Controls_Manager::COLOR,
+				'scheme'    => [
+					'type'  => Schemes\Color::get_type(),
+					'value' => Schemes\Color::COLOR_1,
+				],
+				'selectors' => [
+					'{{WRAPPER}}.elementor-widget-heading .elementor-heading-title' => 'color: {{VALUE}};',
+				],
+			]
+		);
 
-        $this->add_control(
-            'title_color',
-            [
-                'label'     => __('Text Color', 'header-footer-elementor'),
-                'type'      => Controls_Manager::COLOR,
-                'scheme'    => [
-                    'type'  => Schemes\Color::get_type(),
-                    'value' => Schemes\Color::COLOR_1,
-                ],
-                'selectors' => [
-                    '{{WRAPPER}}.elementor-widget-heading .elementor-heading-title' => 'color: {{VALUE}};',
-                ],
-            ]
-        );
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			[
+				'name'     => 'typography',
+				'scheme'   => Schemes\Typography::TYPOGRAPHY_1,
+				'selector' => '{{WRAPPER}} .elementor-heading-title',
+			]
+		);
 
-        $this->add_group_control(
-            Group_Control_Typography::get_type(),
-            [
-                'name'     => 'typography',
-                'scheme'   => Schemes\Typography::TYPOGRAPHY_1,
-                'selector' => '{{WRAPPER}} .elementor-heading-title',
-            ]
-        );
+		$this->add_group_control(
+			Group_Control_Text_Shadow::get_type(),
+			[
+				'name'     => 'text_shadow',
+				'selector' => '{{WRAPPER}} .elementor-heading-title',
+			]
+		);
 
-        $this->add_group_control(
-            Group_Control_Text_Shadow::get_type(),
-            [
-                'name'     => 'text_shadow',
-                'selector' => '{{WRAPPER}} .elementor-heading-title',
-            ]
-        );
+		$this->add_control(
+			'blend_mode',
+			[
+				'label'     => __( 'Blend Mode', 'header-footer-elementor' ),
+				'type'      => Controls_Manager::SELECT,
+				'options'   => [
+					''            => __( 'Normal', 'header-footer-elementor' ),
+					'multiply'    => 'Multiply',
+					'screen'      => 'Screen',
+					'overlay'     => 'Overlay',
+					'darken'      => 'Darken',
+					'lighten'     => 'Lighten',
+					'color-dodge' => 'Color Dodge',
+					'saturation'  => 'Saturation',
+					'color'       => 'Color',
+					'difference'  => 'Difference',
+					'exclusion'   => 'Exclusion',
+					'hue'         => 'Hue',
+					'luminosity'  => 'Luminosity',
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-heading-title' => 'mix-blend-mode: {{VALUE}}',
+				],
+				'separator' => 'none',
+			]
+		);
 
-        $this->add_control(
-            'blend_mode',
-            [
-                'label'     => __('Blend Mode', 'header-footer-elementor'),
-                'type'      => Controls_Manager::SELECT,
-                'options'   => [
-                    ''            => __('Normal', 'header-footer-elementor'),
-                    'multiply'    => 'Multiply',
-                    'screen'      => 'Screen',
-                    'overlay'     => 'Overlay',
-                    'darken'      => 'Darken',
-                    'lighten'     => 'Lighten',
-                    'color-dodge' => 'Color Dodge',
-                    'saturation'  => 'Saturation',
-                    'color'       => 'Color',
-                    'difference'  => 'Difference',
-                    'exclusion'   => 'Exclusion',
-                    'hue'         => 'Hue',
-                    'luminosity'  => 'Luminosity',
-                ],
-                'selectors' => [
-                    '{{WRAPPER}} .elementor-heading-title' => 'mix-blend-mode: {{VALUE}}',
-                ],
-                'separator' => 'none',
-            ]
-        );
+		$this->end_controls_section();
+	}
 
-        $this->end_controls_section();
-    }
+	/**
+	 * Render Page Title output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function page_title() {
+		$page_title = get_the_title();
 
-    /**
-     * Render heading widget output on the frontend.
-     *
-     * Written in PHP and used to generate the final HTML.
-     *
-     * @since 1.0.0
-     * @access protected
-     */
-    protected function render()
-    {
-        $settings = $this->get_settings_for_display();
+		return $page_title;
+	}
 
-        if ('' === $settings['title']) {
-            return;
-        }
+	/**
+	 * Render heading widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
 
-        $this->add_render_attribute('title', 'class', 'elementor-heading-title');
+		if ( '' === $settings['title'] ) {
+			return;
+		}
 
-        if (! empty($settings['size'])) {
-            $this->add_render_attribute('title', 'class', 'elementor-size-' . $settings['size']);
-        }
+		$this->add_render_attribute( 'title', 'class', 'elementor-heading-title' );
 
-        $this->add_inline_editing_attributes('title');
+		if ( ! empty( $settings['size'] ) ) {
+			$this->add_render_attribute( 'title', 'class', 'elementor-size-' . $settings['size'] );
+		}
 
-        $title = $settings['title'];
+		$this->add_inline_editing_attributes( 'title' );
 
-        if (! empty($settings['link']['url'])) {
-            $this->add_render_attribute('url', 'href', $settings['link']['url']);
+		$title = $settings['title'];
 
-            if ($settings['link']['is_external']) {
-                $this->add_render_attribute('url', 'target', '_blank');
-            }
+		if ( ! empty( $settings['link']['url'] ) ) {
+			$this->add_render_attribute( 'url', 'href', $settings['link']['url'] );
 
-            if (! empty($settings['link']['nofollow'])) {
-                $this->add_render_attribute('url', 'rel', 'nofollow');
-            }
+			if ( $settings['link']['is_external'] ) {
+				$this->add_render_attribute( 'url', 'target', '_blank' );
+			}
 
-            $title = sprintf('<a %1$s>%2$s</a>', $this->get_render_attribute_string('url'), $title);
-        }
+			if ( ! empty( $settings['link']['nofollow'] ) ) {
+				$this->add_render_attribute( 'url', 'rel', 'nofollow' );
+			}
 
-        $title_html = sprintf('<%1$s %2$s>%3$s</%1$s>', $settings['header_size'], $this->get_render_attribute_string('title'), $title);
+			$title = sprintf( '<a %1$s>%2$s</a>', $this->get_render_attribute_string( 'url' ), $title );
+		}
 
-        echo $title_html;
-    }
+		$title_html = sprintf( '<%1$s %2$s>%3$s</%1$s>', $settings['header_size'], $this->get_render_attribute_string( 'title' ), $title );
 
-    /**
-     * Render heading widget output in the editor.
-     *
-     * Written as a Backbone JavaScript template and used to generate the live preview.
-     *
-     * @since 1.0.0
-     * @access protected
-     */
-    protected function _content_template()
-    {
-        ?>
-        <#
-        var title = settings.title;
+		echo $title_html;
+	}
 
-        if ( '' !== settings.link.url ) {
-            title = '<a href="' + settings.link.url + '">' + title + '</a>';
-        }
+	/**
+	 * Render heading widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function _content_template() {
+		?>
+		<#
+		var title = settings.title;
 
-        view.addRenderAttribute( 'title', 'class', [ 'elementor-heading-title', 'elementor-size-' + settings.size ] );
+		if ( '' !== settings.link.url ) {
+			title = '<a href="' + settings.link.url + '">' + title + '</a>';
+		}
 
-        view.addInlineEditingAttributes( 'title' );
+		view.addRenderAttribute( 'title', 'class', [ 'elementor-heading-title', 'elementor-size-' + settings.size ] );
 
-        var title_html = '<' + settings.header_size  + ' ' + view.getRenderAttributeString( 'title' ) + '>' + title + '</' + settings.header_size + '>';
+		view.addInlineEditingAttributes( 'title' );
 
-        print( title_html );
-        #>
-        <?php
-    }
+		var title_html = '<' + settings.header_size  + ' ' + view.getRenderAttributeString( 'title' ) + '>' + title + '</' + settings.header_size + '>';
+
+		print( title_html );
+		#>
+		<?php
+	}
 }
