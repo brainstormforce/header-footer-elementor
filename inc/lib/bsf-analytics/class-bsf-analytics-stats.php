94308dd7a623c8fae6c854830b23fd11200d274b
Nikhil Chavan <email@nikhilchavan.com>
2020-01-03 12:45:02 +0530

    Add polyfill for function wp_timezone_string()

--- inc/lib/bsf-analytics/class-bsf-analytics-stats.php
+++ inc/lib/bsf-analytics/class-bsf-analytics-stats.php
@@ -132,3 +132,27 @@ class BSF_Analytics_Stats {
 	}
 
 }
+
+/**
+ * Polyfill for sites using WP version less than 5.3
+ */
+if ( ! function_exists( 'wp_timezone_string' ) ) {
+	function wp_timezone_string() {
+		$timezone_string = get_option( 'timezone_string' );
+	 
+		if ( $timezone_string ) {
+			return $timezone_string;
+		}
+	 
+		$offset  = (float) get_option( 'gmt_offset' );
+		$hours   = (int) $offset;
+		$minutes = ( $offset - $hours );
+	 
+		$sign      = ( $offset < 0 ) ? '-' : '+';
+		$abs_hour  = abs( $hours );
+		$abs_mins  = abs( $minutes * 60 );
+		$tz_offset = sprintf( '%s%02d:%02d', $sign, $abs_hour, $abs_mins );
+	 
+		return $tz_offset;
+	}
+}
