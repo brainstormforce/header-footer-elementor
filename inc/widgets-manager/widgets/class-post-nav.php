d4892990a8de39e7012e34ceb7185c91518a08b0
Prajakta Suryawanshi <prajaktas@bsf.io>
2020-01-06 16:16:30 +0530

    Added the basic widget post nav

---
+++ inc/widgets-manager/widgets/class-post-nav.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Elementor Classes.
+ *
+ * @package header-footer-elementor
+ */
+
+namespace HFE\WidgetsManager\Widgets;
+
+use Elementor\Controls_Manager;
+use Elementor\Control_Media;
+use Elementor\Utils;
+use Elementor\Group_Control_Border;
+use Elementor\Group_Control_Typography;
+use Elementor\Group_Control_Box_Shadow;
+use Elementor\Scheme_Typography;
+use Elementor\Scheme_Color;
+use Elementor\Group_Control_Image_Size;
+use Elementor\Repeater;
+use Elementor\Group_Control_Css_Filter;
+use Elementor\Group_Control_Text_Shadow;
+use Elementor\Plugin;
+use Elementor\Widget_Base;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;   // Exit if accessed directly.
+}
+
+/**
+ * HFE Post Navigation widget.
+ *
+ * HFE widget for Post Navigation.
+ *
+ * @since x.x.x
+ */
+class Post_Nav extends Widget_Base {
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since x.x.x
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'post-nav';
+	}
+
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since x.x.x
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Post Navigation', 'header-footer-elementor' );
+	}
+
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since x.x.x
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'fas fa-search';
+	}
+
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since x.x.x
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'hfe-widgets' ];
+	}
+}
