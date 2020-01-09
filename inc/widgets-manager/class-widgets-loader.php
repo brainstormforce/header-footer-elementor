b4fb4edcaaf0b02d31956ed4ed582c89a21a52c2
SuryawanshiPrajakta <patilprajakta14697@gmail.com>
2020-01-09 13:14:55 +0530

    Fixed the merge conflict

--- inc/widgets-manager/class-widgets-loader.php
+++ inc/widgets-manager/class-widgets-loader.php
@@ -26,7 +26,7 @@ class Widgets_Loader {
 	 * @since  1.2.0
 	 * @var null
 	 */
-	private static $_instance = null;
+private static $_instance = null;
 
 	/**
 	 * Get instance of Widgets_Loader
@@ -34,30 +34,30 @@ class Widgets_Loader {
 	 * @since  1.2.0
 	 * @return Widgets_Loader
 	 */
-	public static function instance() {
-		if ( ! isset( self::$_instance ) ) {
-			self::$_instance = new self();
-		}
-
-		return self::$_instance;
+public static function instance() {
+	if ( ! isset( self::$_instance ) ) {
+		self::$_instance = new self();
 	}
+
+	return self::$_instance;
+}
 
 	/**
 	 * Setup actions and filters.
 	 *
 	 * @since  1.2.0
 	 */
-	private function __construct() {
+private function __construct() {
 
-		// Register category.
-		add_action( 'elementor/elements/categories_registered', [ $this, 'register_widget_category' ] );
+	// Register category.
+	add_action( 'elementor/elements/categories_registered', [ $this, 'register_widget_category' ] );
 
-		// Register widgets.
-		add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ] );
+	// Register widgets.
+	add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ] );
 
-		// Add svg support.
-		add_filter( 'upload_mimes', [ $this, 'hfe_svg_mime_types' ] );
-	}
+	// Add svg support.
+	add_filter( 'upload_mimes', [ $this, 'hfe_svg_mime_types' ] );
+}
 
 
 	/**
@@ -68,14 +68,14 @@ class Widgets_Loader {
 	 * @since 1.2.0
 	 * @access public
 	 */
-	public function include_widgets_files() {
-		require_once HFE_DIR . '/inc/widgets-manager/widgets/class-retina.php';
-		require_once HFE_DIR . '/inc/widgets-manager/widgets/class-copyright.php';
-		require_once HFE_DIR . '/inc/widgets-manager/widgets/class-copyright-shortcode.php';
-		require_once HFE_DIR . '/inc/widgets-manager/widgets/class-post-nav.php';
-		require_once HFE_DIR . '/inc/widgets-manager/widgets/class-post-title.php';
-		require_once HFE_DIR . '/inc/widgets-manager/widgets/class-site-title.php';
-	}
+public function include_widgets_files() {
+	require_once HFE_DIR . '/inc/widgets-manager/widgets/class-retina.php';
+	require_once HFE_DIR . '/inc/widgets-manager/widgets/class-copyright.php';
+	require_once HFE_DIR . '/inc/widgets-manager/widgets/class-copyright-shortcode.php';
+	require_once HFE_DIR . '/inc/widgets-manager/widgets/class-post-nav.php';
+	require_once HFE_DIR . '/inc/widgets-manager/widgets/class-post-title.php';
+	require_once HFE_DIR . '/inc/widgets-manager/widgets/class-site-title.php';
+}
 
 	/**
 	 * Provide the SVG support for Retina Logo widget.
@@ -85,12 +85,12 @@ class Widgets_Loader {
 	 * @since  1.2.0
 	 * @return $mimes.
 	 */
-	public function hfe_svg_mime_types( $mimes ) {
+public function hfe_svg_mime_types( $mimes ) {
 
-		// New allowed mime types.
-		$mimes['svg'] = 'image/svg+xml';
-		return $mimes;
-	}
+	// New allowed mime types.
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+}
 
 	/**
 	 * Register Category
@@ -98,19 +98,19 @@ class Widgets_Loader {
 	 * @since 1.2.0
 	 * @param object $this_cat class.
 	 */
-	public function register_widget_category( $this_cat ) {
-		$category = __( 'Header, Footer & Blocks', 'header-footer-elementor' );
+public function register_widget_category( $this_cat ) {
+	$category = __( 'Header, Footer & Blocks', 'header-footer-elementor' );
 
-		$this_cat->add_category(
-			'hfe-widgets',
-			[
-				'title' => $category,
-				'icon'  => 'eicon-font',
-			]
-		);
+	$this_cat->add_category(
+		'hfe-widgets',
+		[
+			'title' => $category,
+			'icon'  => 'eicon-font',
+		]
+	);
 
-		return $this_cat;
-	}
+	return $this_cat;
+}
 
 	/**
 	 * Register Widgets
@@ -120,19 +120,16 @@ class Widgets_Loader {
 	 * @since 1.2.0
 	 * @access public
 	 */
-	public function register_widgets() {
+public function register_widgets() {
 
 		// Its is now safe to include Widgets files.
 		$this->include_widgets_files();
 		// Register Widgets.
 		Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Retina() );
 		Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Copyright() );
-<<<<<<< HEAD
 		Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Post_Nav() );
-=======
 		Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Post_Title() );
 		Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Site_Title() );
->>>>>>> 74310bf75f3e6919684888c4aad3cb8186625e50
 	}
 
 }
