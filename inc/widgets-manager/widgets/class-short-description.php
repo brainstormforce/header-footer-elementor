62e85c67523f9a9626c9106ed9d5e1b1964e360e
pratikshasaman <pratikshas@bsf.io>
2020-01-17 14:12:22 +0530

    Version number updated

--- inc/widgets-manager/widgets/class-short-description.php
+++ inc/widgets-manager/widgets/class-short-description.php
@@ -228,7 +228,7 @@ class Short_Description extends Widget_Base {
 	 *
 	 * Written as a Backbone JavaScript template and used to generate the live preview.
 	 *
-	 * @since 1.2.0
+	 * @since x.x.x
 	 * @access protected
 	 */
 	protected function _content_template() {}
