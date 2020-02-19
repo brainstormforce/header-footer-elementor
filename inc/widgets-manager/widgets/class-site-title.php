ff8dffaa3bdef958fb81c16cc8e7f5ec9d53f7bb
SuryawanshiPrajakta <patilprajakta14697@gmail.com>
2020-02-19 16:43:08 +0530

    Added icon option and fixed the href issue

--- inc/widgets-manager/widgets/class-site-title.php
+++ inc/widgets-manager/widgets/class-site-title.php
@@ -114,7 +114,7 @@ class Site_Title extends Widget_Base {
 		$this->add_control(
 			'before',
 			[
-				'label'   => __( 'Before Title text', 'header-footer-elementor' ),
+				'label'   => __( 'Before Title Text', 'header-footer-elementor' ),
 				'type'    => Controls_Manager::TEXTAREA,
 				'rows'    => '1',
 				'dynamic' => [
@@ -134,6 +134,95 @@ class Site_Title extends Widget_Base {
 				],
 			]
 		);
+		
+		$this->add_control(
+			'icon',
+			[
+				'label'       => __( 'Icon', 'header-footer-elementor' ),
+				'type'        => Controls_Manager::ICONS,
+				'label_block' => 'true',
+			]
+		);
+	
+		// $this->add_responsive_control(
+		// 	'icon_size',
+		// 	array(
+		// 		'label'      => __( 'Icon Size', 'header-footer-elementor' ),
+		// 		'type'       => Controls_Manager::SLIDER,
+		// 		'size_units' => array( 'px', 'em', 'rem' ),
+		// 		'range'      => array(
+		// 			'px' => array(
+		// 				'min' => 1,
+		// 				'max' => 200,
+		// 			),
+		// 		),
+		// 		'default'    => array(
+		// 			'size' => 40,
+		// 			'unit' => 'px',
+		// 		),
+		// 		'condition' => array(
+		// 			'icon[value]!' => '' 
+		// 		),
+		// 		'selectors'  => array(
+		// 			'{{WRAPPER}} .hfe-icon i' => 'font-size: {{SIZE}}{{UNIT}}; height: {{SIZE}}{{UNIT}}; width: {{SIZE}}{{UNIT}};line-height: {{SIZE}}{{UNIT}}; text-align: center;',
+		// 			'{{WRAPPER}} .hfe-icon' => ' height: {{SIZE}}{{UNIT}}; width: {{SIZE}}{{UNIT}};line-height: {{SIZE}}{{UNIT}};',
+		// 		),
+		// 	)
+		// );
+
+		$this->add_control(
+			'icon_indent',
+			array(
+				'label'      => __( 'Icon Spacing', 'uael' ),
+				'type'       => Controls_Manager::SLIDER,
+				'range'      => array(
+					'px' => array(
+						'max' => 50,
+					),
+				),
+				'condition' => array(
+					'icon[value]!' => '' 
+				),
+				'selectors'  => array(
+					'{{WRAPPER}} .hfe-icon' => 'margin-right: {{SIZE}}{{UNIT}};',
+				),
+			)
+		);
+
+		$this->add_control(
+			'icon_color',
+			array(
+				'label'      => __( 'Icon Color', 'uael' ),
+				'type'       => Controls_Manager::COLOR,
+				'scheme'     => array(
+					'type'  => Scheme_Color::get_type(),
+					'value' => Scheme_Color::COLOR_1,
+				),
+				'condition' => array(
+					'icon[value]!' => '' 
+				),
+				'default'    => '',
+				'selectors'  => array(
+					'{{WRAPPER}} .hfe-icon i' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .hfe-icon svg' => 'fill: {{VALUE}};',
+				),
+			)
+		);
+		$this->add_control(
+			'icons_hover_color',
+			array(
+				'label'      => __( 'Icon Hover Color', 'uael' ),
+				'type'       => Controls_Manager::COLOR,
+				'condition' => array(
+					'icon[value]!' => '' 
+				),
+				'default'    => '',
+				'selectors'  => array(
+					'{{WRAPPER}} .hfe-icon:hover i'=> 'color: {{VALUE}};',
+					'{{WRAPPER}} .hfe-icon:hover svg' => 'fill: {{VALUE}};',
+				),
+			)
+		);
 
 		$this->add_control(
 			'custom_link',
@@ -142,7 +231,7 @@ class Site_Title extends Widget_Base {
 				'type'    => Controls_Manager::SELECT,
 				'default' => 'toggle',
 				'options' => [
-					'custom'  => __( 'Custom', 'header-footer-elementor' ),
+					'custom'  => __( 'Custom URL', 'header-footer-elementor' ),
 					'default' => __( 'Default', 'header-footer-elementor' ),
 				],
 				'default' => 'default',
@@ -345,26 +434,31 @@ class Site_Title extends Widget_Base {
 
 		<div class="hfe-module-content hfe-heading-wrapper elementor-widget-heading">
 		<?php if ( ! empty( $settings['heading_link']['url'] ) && 'custom' === $settings['custom_link'] ) { ?>
-					<a <?php echo esc_url_raw( $link ); ?> >
+					<a <?php echo esc_attr( $link ); ?> >
 				<?php } else { ?>
 					<a href="<?php echo get_home_url(); ?>">
 				<?php } ?>
-			<<?php echo wp_kses_post( $settings['heading_tag'] ); ?> class="hfe-heading">
-						<span class="hfe-heading-text elementor-heading-title elementor-size-<?php echo $settings['size']; ?>" >
-						<?php
-						if ( '' !== $settings['before'] ) {
-							echo wp_kses_post( $settings['before'] );
-						}
-						echo wp_kses_post( $title );
-						if ( '' !== $settings['after'] ) {
-							echo wp_kses_post( $settings['after'] );
-						}
-						?>
-						</span>
-						<?php if ( ! empty( $settings['heading_link']['url'] ) ) { ?>
-					</a>	
-					<?php } ?>					
+			<<?php echo wp_kses_post( $settings['heading_tag'] ); ?> class="hfe-heading elementor-heading-title">
+				<?php if( '' !== $settings['icon']['value'] ){ ?>
+					<span class="hfe-icon">
+						<?php \Elementor\Icons_Manager::render_icon( $settings['icon'], array( 'aria-hidden' => 'true' ) ); ?>					
+					</span>
+				<?php } ?>
+					<span class="hfe-heading-text elementor-size-<?php echo $settings['size']; ?>" >
+					<?php
+					if ( '' !== $settings['before'] ) {
+						echo wp_kses_post( $settings['before'] ) . ' ';
+					}
+					echo wp_kses_post( $title );
+					if ( '' !== $settings['after'] ) {
+						echo ' ' . wp_kses_post( $settings['after'] );
+					}
+					?>
+					</span>
+					<?php if ( ! empty( $settings['heading_link']['url'] ) ) { ?>
+				<?php } ?>					
 			</<?php echo wp_kses_post( $settings['heading_tag'] ); ?>>
+			</a>	
 		</div>
 		<?php
 	}
@@ -388,19 +482,25 @@ class Site_Title extends Widget_Base {
 		if ( '' != settings.heading_link.url ) {
 			view.addRenderAttribute( 'url', 'href', settings.heading_link.url );
 		}
+		var iconHTML = elementor.helpers.renderIcon( view, settings.icon, { 'aria-hidden': true }, 'i' , 'object' );
 		#>
 		<div class="hfe-module-content hfe-heading-wrapper elementor-widget-heading">
-			<{{{ settings.heading_tag }}} class="hfe-heading">
+			<{{{ settings.heading_tag }}} class="hfe-heading elementor-heading-title">
 				<# if ( '' != settings.heading_link.url ) { #>
 					<a {{{ view.getRenderAttributeString( 'url' ) }}} >
 				<# } #>
+				<# if( '' != settings.icon.value ){ #>
+					<span class="hfe-icon">
+						{{{iconHTML.value}}}					
+					</span>
+				<# } #>
 				<span class="hfe-heading-text  elementor-heading-title elementor-size-{{{ settings.size }}}" data-elementor-setting-key="heading_title" data-elementor-inline-editing-toolbar="basic" >
 				<#if ( '' != settings.before ){#>
-					{{{settings.before}}}
+					{{{ settings.before + ' ' }}} 
 				<#}#>
 				<?php echo wp_kses_post( get_bloginfo( 'name' ) ); ?>
 				<# if ( '' != settings.after ){#>
-					{{{settings.after}}}
+				    {{{ ' ' + settings.after }}}
 				<#}#>
 				</span>
 				<# if ( '' != settings.heading_link.url ) { #>
