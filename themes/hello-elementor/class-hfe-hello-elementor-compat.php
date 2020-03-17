15d7ffd5ebb8e2ea3d270b847ad9c182417cfffc
Elvina <elvinag@bsf.io>
2020-03-17 19:07:58 +0530

    Added support to hello elementor

---
+++ themes/hello-elementor/class-hfe-hello-elementor-compat.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * HFE_Hello_Elementor_Compat setup
+ *
+ * @package header-footer-elementor
+ */
+
+/**
+ * Hello Elementor compatibility.
+ */
+class HFE_Hello_Elementor_Compat {
+
+	/**
+	 * Instance of HFE_Hello_Elementor_Compat.
+	 *
+	 * @var HFE_Hello_Elementor_Compat
+	 */
+	private static $instance;
+
+	/**
+	 *  Initiator
+	 */
+	public static function instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new HFE_Hello_Elementor_Compat();
+
+			require_once HFE_DIR . 'themes/default/class-hfe-default-compat.php';
+		}
+
+		return self::$instance;
+	}
+}
+
+HFE_Hello_Elementor_Compat::instance();
