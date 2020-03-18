bf9030803d0d5429a30510f323609c5ccaf02f9c
Elvina <elvinag@bsf.io>
2020-03-18 19:49:46 +0530

    Remaining some logic

--- inc/js/hfe-footer.js
+++ inc/js/hfe-footer.js
@@ -13,10 +13,18 @@
 			elementor.on( "document:loaded", function() {
 
                 setTimeout( function() {
-                    console.log(elementorFrontend.documentsManager.documents);
+                    //console.log(elementorFrontend.documentsManager.documents);
                     jQuery.each(elementorFrontend.documentsManager.documents, function (index, document) {
                         var $documentElement = document.$element;
-                        console.log($documentElement.data('elementor-title'));
+                        //console.log($documentElement.data('elementor-title'));
+                        var json_parse = JSON.parse( hfe_admin.json_array );
+           				var dummy_id = $documentElement.data('elementor-id');
+                        
+                        console.log( json_parse );
+
+                        json_parse.forEach( function(item, index){
+                        	console.log( json_parse[index].ID );
+                        } );
                         // Update this selector - elementor-document-handle__title text.
                     });
                 }, 1000 );
