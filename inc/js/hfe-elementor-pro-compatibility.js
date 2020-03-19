2cfaffd9e06849895a893470c95354b627750994
Prajakta <patilprajakta14697@gmail.com>
2020-03-19 14:40:27 -0700

    Removed the JS Spacing

--- inc/js/hfe-elementor-pro-compatibility.js
+++ inc/js/hfe-elementor-pro-compatibility.js
@@ -12,11 +12,11 @@
 		init: function() {
 			elementor.on( "document:loaded", function() {
                 setTimeout( function() {
-                    jQuery.each(elementorFrontend.documentsManager.documents, function (index, document) {
+                    jQuery.each( elementorFrontend.documentsManager.documents, function ( index, document ) {
                         var $documentElement = document.$element;
                         var ids_array = JSON.parse( hfe_admin.ids_array );
-                        ids_array.forEach( function(item, index){
-                        	var elementor_id = $documentElement.data('elementor-id');
+                        ids_array.forEach( function( item, index ){
+                        	var elementor_id = $documentElement.data( 'elementor-id' );
                         	if( elementor_id == ids_array[index].id ){
                         		$documentElement.find( '.elementor-document-handle__title' ).text( elementor.translate('edit_element', [ids_array[index].value] ) );
                         	}
