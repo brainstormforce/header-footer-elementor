27c92c15b26e79a8ecb9fb45a08febfba26bac99
Vikas <vikasp@bsf.io>
2020-03-23 15:15:34 +0530

    Added class exists check for BSF_Analytics_Stats class

--- inc/lib/bsf-analytics/class-bsf-analytics-stats.php
+++ inc/lib/bsf-analytics/class-bsf-analytics-stats.php
@@ -1,141 +1,143 @@
 <?php
 
-class BSF_Analytics_Stats {
+if( ! class_exists( 'BSF_Analytics_Stats' ) ) {
+	class BSF_Analytics_Stats {
 
-	/**
-	 * Active plugins.
-	 *
-	 * Holds the sites active plugins list.
-	 *
-	 * @var array
-	 */
-	private $plugins;
+		/**
+		 * Active plugins.
+		 *
+		 * Holds the sites active plugins list.
+		 *
+		 * @var array
+		 */
+		private $plugins;
 
-	private static $_instance = null;
+		private static $_instance = null;
 
-	public static function instance() {
-		if ( null === self::$_instance ) {
-			self::$_instance = new self();
-		}
-
-		return self::$_instance;
-	}
-
-	public function get_stats() {
-		return apply_filters( 'bsf_core_stats', $this->get_default_stats() );
-	}
-
-	private function get_default_stats() {
-		return array(
-			'graupi_version'         => defined( 'BSF_UPDATER_VERSION' ) ? BSF_UPDATER_VERSION : false,
-			'domain_name'            => get_site_url(),
-			'php_os'                 => PHP_OS,
-			'server_software'        => $_SERVER['SERVER_SOFTWARE'],
-			'mysql_version'          => $this->get_mysql_version(),
-			'php_version'            => $this->get_php_version(),
-			'php_max_input_vars'     => ini_get( 'max_input_vars' ), // phpcs:ignore:PHPCompatibility.IniDirectives.NewIniDirectives.max_input_varsFound
-			'php_post_max_size'      => ini_get( 'post_max_size' ),
-			'php_max_execution_time' => ini_get( 'max_execution_time' ),
-			'php_memory_limit'       => ini_get( 'memory_limit' ),
-			'zip_installed'          => extension_loaded( 'zip' ),
-			'imagick_availabile'     => extension_loaded( 'imagick' ),
-			'xmlreader_exists'       => class_exists( 'XMLReader' ),
-			'gd_available'           => extension_loaded( 'gd' ),
-			'curl_version'           => $this->get_curl_version(),
-			'curl_ssl_version'       => $this->get_curl_ssl_version(),
-			'is_writable'            => $this->is_content_writable(),
-
-			'wp_version'             => get_bloginfo( 'version' ),
-			'user_count'             => $this->get_user_count(),
-			'site_language'          => get_locale(),
-			'timezone'               => wp_timezone_string(),
-			'is_ssl'                 => is_ssl(),
-			'is_multisite'           => is_multisite(),
-			'external_object_cache'  => (bool) wp_using_ext_object_cache(),
-			'wp_debug'               => WP_DEBUG,
-			'wp_debug_display'       => WP_DEBUG_DISPLAY,
-			'script_debug'           => SCRIPT_DEBUG,
-
-			'active_plugins'         => $this->get_active_plugins(),
-
-			'active_theme'           => get_template(),
-			'active_stylesheet'      => get_stylesheet(),
-		);
-	}
-
-	private function get_php_version() {
-		if ( defined( 'PHP_MAJOR_VERSION' ) && defined( 'PHP_MINOR_VERSION' ) && defined( 'PHP_RELEASE_VERSION' ) ) { // phpcs:ignore
-			return PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION . '.' . PHP_RELEASE_VERSION;
-		}
-
-		return phpversion();
-	}
-
-	private function get_user_count() {
-		if ( is_multisite() ) {
-			$user_count = get_user_count();
-		} else {
-			$count      = count_users();
-			$user_count = $count['total_users'];
-		}
-
-		return $user_count;
-	}
-
-	private function get_active_plugins() {
-		if ( ! $this->plugins ) {
-			// Ensure get_plugin_data function is loaded
-			if ( ! function_exists( 'get_plugin_data' ) ) {
-				require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		public static function instance() {
+			if ( null === self::$_instance ) {
+				self::$_instance = new self();
 			}
-	
-			$plugins = wp_get_active_and_valid_plugins();
-			$plugins = array_map( 'get_plugin_data', $plugins );
-			$this->plugins = array_map( array( $this, 'format_plugin' ), $plugins );
+
+			return self::$_instance;
 		}
 
-		return $this->plugins;
-	}
-
-	public function format_plugin( $plugin ) {
-		return array(
-			'name'    		=> html_entity_decode( $plugin['Name'] ),
-			'url'     		=> $plugin['PluginURI'],
-			'version' 		=> $plugin['Version'],
-			'slug'    		=> $plugin['TextDomain'],
-			'author_name'  	=> html_entity_decode( strip_tags( $plugin['Author'] ) ),
-			'author_url'  	=> $plugin['AuthorURI'],
-		);
-	}
-
-	private function get_curl_ssl_version() {
-		$curl = array();
-		if( function_exists( 'curl_version' ) ) {
-			$curl = curl_version();
+		public function get_stats() {
+			return apply_filters( 'bsf_core_stats', $this->get_default_stats() );
 		}
 
-		return isset( $curl['ssl_version'] ) ? $curl['ssl_version'] : false;
-	}
+		private function get_default_stats() {
+			return array(
+				'graupi_version'         => defined( 'BSF_UPDATER_VERSION' ) ? BSF_UPDATER_VERSION : false,
+				'domain_name'            => get_site_url(),
+				'php_os'                 => PHP_OS,
+				'server_software'        => $_SERVER['SERVER_SOFTWARE'],
+				'mysql_version'          => $this->get_mysql_version(),
+				'php_version'            => $this->get_php_version(),
+				'php_max_input_vars'     => ini_get( 'max_input_vars' ), // phpcs:ignore:PHPCompatibility.IniDirectives.NewIniDirectives.max_input_varsFound
+				'php_post_max_size'      => ini_get( 'post_max_size' ),
+				'php_max_execution_time' => ini_get( 'max_execution_time' ),
+				'php_memory_limit'       => ini_get( 'memory_limit' ),
+				'zip_installed'          => extension_loaded( 'zip' ),
+				'imagick_availabile'     => extension_loaded( 'imagick' ),
+				'xmlreader_exists'       => class_exists( 'XMLReader' ),
+				'gd_available'           => extension_loaded( 'gd' ),
+				'curl_version'           => $this->get_curl_version(),
+				'curl_ssl_version'       => $this->get_curl_ssl_version(),
+				'is_writable'            => $this->is_content_writable(),
 
-	private function get_curl_version() {
-		$curl = array();
-		if( function_exists( 'curl_version' ) ) {
-			$curl = curl_version();
+				'wp_version'             => get_bloginfo( 'version' ),
+				'user_count'             => $this->get_user_count(),
+				'site_language'          => get_locale(),
+				'timezone'               => wp_timezone_string(),
+				'is_ssl'                 => is_ssl(),
+				'is_multisite'           => is_multisite(),
+				'external_object_cache'  => (bool) wp_using_ext_object_cache(),
+				'wp_debug'               => WP_DEBUG,
+				'wp_debug_display'       => WP_DEBUG_DISPLAY,
+				'script_debug'           => SCRIPT_DEBUG,
+
+				'active_plugins'         => $this->get_active_plugins(),
+
+				'active_theme'           => get_template(),
+				'active_stylesheet'      => get_stylesheet(),
+			);
 		}
 
-		return isset( $curl['version'] ) ? $curl['version'] : false;
-	}
+		private function get_php_version() {
+			if ( defined( 'PHP_MAJOR_VERSION' ) && defined( 'PHP_MINOR_VERSION' ) && defined( 'PHP_RELEASE_VERSION' ) ) { // phpcs:ignore
+				return PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION . '.' . PHP_RELEASE_VERSION;
+			}
 
-	private function get_mysql_version() {
-		global $wpdb;
-		return $wpdb->db_version();
-	}
+			return phpversion();
+		}
 
-	private function is_content_writable() {
-		$upload_dir = wp_upload_dir();
-		return wp_is_writable( $upload_dir['basedir'] );
-	}
+		private function get_user_count() {
+			if ( is_multisite() ) {
+				$user_count = get_user_count();
+			} else {
+				$count      = count_users();
+				$user_count = $count['total_users'];
+			}
 
+			return $user_count;
+		}
+
+		private function get_active_plugins() {
+			if ( ! $this->plugins ) {
+				// Ensure get_plugin_data function is loaded
+				if ( ! function_exists( 'get_plugin_data' ) ) {
+					require_once ABSPATH . 'wp-admin/includes/plugin.php';
+				}
+		
+				$plugins = wp_get_active_and_valid_plugins();
+				$plugins = array_map( 'get_plugin_data', $plugins );
+				$this->plugins = array_map( array( $this, 'format_plugin' ), $plugins );
+			}
+
+			return $this->plugins;
+		}
+
+		public function format_plugin( $plugin ) {
+			return array(
+				'name'    		=> html_entity_decode( $plugin['Name'] ),
+				'url'     		=> $plugin['PluginURI'],
+				'version' 		=> $plugin['Version'],
+				'slug'    		=> $plugin['TextDomain'],
+				'author_name'  	=> html_entity_decode( strip_tags( $plugin['Author'] ) ),
+				'author_url'  	=> $plugin['AuthorURI'],
+			);
+		}
+
+		private function get_curl_ssl_version() {
+			$curl = array();
+			if( function_exists( 'curl_version' ) ) {
+				$curl = curl_version();
+			}
+
+			return isset( $curl['ssl_version'] ) ? $curl['ssl_version'] : false;
+		}
+
+		private function get_curl_version() {
+			$curl = array();
+			if( function_exists( 'curl_version' ) ) {
+				$curl = curl_version();
+			}
+
+			return isset( $curl['version'] ) ? $curl['version'] : false;
+		}
+
+		private function get_mysql_version() {
+			global $wpdb;
+			return $wpdb->db_version();
+		}
+
+		private function is_content_writable() {
+			$upload_dir = wp_upload_dir();
+			return wp_is_writable( $upload_dir['basedir'] );
+		}
+
+	}
 }
 
 /**
