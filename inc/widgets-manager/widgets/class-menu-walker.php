939bb907685e8f8b675765940d9e01b7f9ba8743
pratiksha samane <pratikshas@bsf.io>
2020-08-13 14:27:01 +0530

    space added

--- inc/widgets-manager/widgets/class-menu-walker.php
+++ inc/widgets-manager/widgets/class-menu-walker.php
@@ -59,8 +59,8 @@ class Menu_Walker extends \Walker_Nav_Menu {
 		$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
 		$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . $rel_xfn . '"' : '' . $rel_blank;
 		$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
-		
-		$attributes .= apply_filters( 'hfe_nav_menu_attrs', array_filter( (array)$attributes ) );
+
+		$attributes .= apply_filters( 'hfe_nav_menu_attrs', array_filter( (array) $attributes ) );
 
 		$item_output  = $args->has_children ? '<div class="hfe-has-submenu-container">' : '';
 		$item_output .= $args->before;
