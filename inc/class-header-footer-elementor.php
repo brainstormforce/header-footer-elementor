681dde9e56bf957bb50aec0f0b0cfb510bfc239b
Sushma Kure <sushmak@bsf.io>
2021-02-10 09:57:35 +0530

    Added settings Page

--- inc/class-header-footer-elementor.php
+++ inc/class-header-footer-elementor.php
@@ -57,6 +57,10 @@ class Header_Footer_Elementor {
 			$this->includes();
 			$this->load_textdomain();
 
+			register_activation_hook( __FILE__, array( $this, 'plugin_activation' ) );
+
+			add_action( 'init', [ $this, 'setup_settings_page' ] );
+
 			if ( 'genesis' == $this->template ) {
 				require HFE_DIR . 'themes/genesis/class-hfe-genesis-compat.php';
 			} elseif ( 'astra' == $this->template ) {
@@ -90,27 +94,19 @@ class Header_Footer_Elementor {
 			add_action( 'admin_notices', [ $this, 'register_notices' ] );
 
 			// BSF Analytics Tracker.
-			if ( ! class_exists( 'BSF_Analytics_Loader' ) ) {
-				require_once HFE_DIR . 'admin/bsf-analytics/class-bsf-analytics-loader.php';
-			}
-
-			$bsf_analytics = BSF_Analytics_Loader::get_instance();
-
-			$bsf_analytics->set_entity(
-				[
-					'bsf' => [
-						'product_name'    => 'Elementor - Header, Footer & Blocks',
-						'path'            => HFE_DIR . 'admin/bsf-analytics',
-						'author'          => 'Brainstorm Force',
-						'time_to_display' => '+24 hours',
-					],
-				]
-			);
+			require_once HFE_DIR . 'admin/bsf-analytics/class-bsf-analytics.php';
 
 		} else {
 			add_action( 'admin_notices', [ $this, 'elementor_not_available' ] );
 			add_action( 'network_admin_notices', [ $this, 'elementor_not_available' ] );
 		}
+	}
+
+	/**
+	 * Call hook on plugin activation for both multi-site and single-site.
+	 */
+	function plugin_activation() {
+		exit( wp_redirect( admin_url( 'edit.php?post_type=elementor-hf' ) ) );
 	}
 
 	/**
@@ -205,7 +201,7 @@ class Header_Footer_Elementor {
 
 			$plugin = 'elementor/elementor.php';
 
-			if ( file_exists( WP_PLUGIN_DIR . '/elementor/elementor.php' ) ) {
+			if ( _is_elementor_installed() ) {
 
 				$action_url   = wp_nonce_url( 'plugins.php?action=activate&amp;plugin=' . $plugin . '&amp;plugin_status=all&amp;paged=1&amp;s', 'activate-plugin_' . $plugin );
 				$button_label = __( 'Activate Elementor', 'header-footer-elementor' );
@@ -233,8 +229,8 @@ class Header_Footer_Elementor {
 		// Load Elementor Canvas Compatibility.
 		require_once HFE_DIR . 'inc/class-hfe-elementor-canvas-compat.php';
 
-		// Load WPML & Polylang Compatibility if WPML is installed and activated.
-		if ( defined( 'ICL_SITEPRESS_VERSION' ) || defined( 'POLYLANG_BASENAME' ) ) {
+		// Load WPML Compatibility if WPML is installed and activated.
+		if ( defined( 'ICL_SITEPRESS_VERSION' ) ) {
 			require_once HFE_DIR . 'inc/compatibility/class-hfe-wpml-compatibility.php';
 		}
 
@@ -336,6 +332,15 @@ class Header_Footer_Elementor {
 		$classes[] = 'ehf-stylesheet-' . get_stylesheet();
 
 		return $classes;
+	}
+	
+	/**
+	 * Display Settings Page options
+	 *
+	 * @since x.x.x
+	 */
+	public function setup_settings_page() {
+		require_once HFE_DIR . 'inc/class-hfe-settings-page.php';
 	}
 
 	/**
@@ -450,4 +455,20 @@ class Header_Footer_Elementor {
 		return self::$elementor_instance->frontend->get_builder_content_for_display( $id );
 	}
 
+}
+/**
+ * Is elementor plugin installed.
+ */
+if ( ! function_exists( '_is_elementor_installed' ) ) {
+
+	/**
+	 * Check if Elementor is installed
+	 *
+	 * @since 1.5.0
+	 *
+	 * @access public
+	 */
+	function _is_elementor_installed() {
+		return defined( 'ELEMENTOR_VERSION' ) ? true : false;
+	}
 }
---
+++ inc/class-hfe-settings-page.php
@@ -0,0 +1,254 @@
+<?php
+/**
+ * HFE Settings Page.
+ *
+ * Add plugin setting page.
+ *
+ * @since x.x.x
+ * @package hfe
+ */
+
+namespace HFE\Themes;
+
+/**
+ * Class Settings Page.
+ *
+ * @since x.x.x
+ */
+class HFE_Settings_Page {
+
+	/**
+	 * Constructor.
+	 *
+	 * @since x.x.x
+	 */
+	public function __construct() {
+		$this->setup_fallback_support();
+		add_action( 'admin_menu', [ $this, 'hfe_register_settings_page' ] );
+		add_action( 'admin_init', [ $this, 'hfe_admin_init' ] );
+		add_action( 'admin_head', [ $this, 'hfe_global_css' ] );
+		add_filter( 'views_edit-elementor-hf', [ $this, 'hfe_settings' ], 10, 1 );
+	}
+
+	/**
+	 * Adds CSS to Hide the extra submenu added for the settings tab.
+	 *
+	 * @since x.x.x
+	 * @return void
+	 */
+	public function hfe_global_css() {
+		wp_enqueue_style( 'hfe-admin-style', HFE_URL . 'admin/assets/css/ehf-admin.css', [], HFE_VER );
+	}
+
+	/**
+	 * Adds a tab in plugin submenu page.
+	 *
+	 * @since x.x.x
+	 * @param string $views to add tab to current post type view.
+	 *
+	 * @return mixed
+	 */
+	public function hfe_settings( $views ) {
+		$this->hfe_tabs();
+		return $views;
+	}
+
+
+	/**
+	 * Function for registering the settings api.
+	 *
+	 * @since x.x.x
+	 * @return void
+	 */
+	public function hfe_admin_init() {
+		register_setting( 'hfe-plugin-options', 'hfe_compatibility_option' );
+		add_settings_section( 'hfe-options', __( 'Add Theme Support', 'header-footer-elementor' ), [ $this, 'hfe_compatibility_callback' ], 'Settings' );
+		add_settings_field( 'hfe-way', 'Methods to Add Theme Support', [ $this, 'hfe_compatibility_option_callback' ], 'Settings', 'hfe-options' );
+	}
+
+	/**
+	 * Call back function for the ssettings api function add_settings_section
+	 *
+	 * This function can be used to add description of the settings sections
+	 *
+	 * @since x.x.x
+	 * @return void
+	 */
+	public function hfe_compatibility_callback() {
+		_e( 'The Elementor - Header, Footer & Blocks plugin need compatibility with your current theme to work smoothly.</br></br>Following are two methods that enable theme support for the plugin.</br></br>Method 1 is selected by default and that works fine almost will all themes. In case, you face any issue with the header or footer template, try choosing Method 2.', 'header-footer-elementor' );
+	}
+
+	/**
+	 * Call back function for the ssettings api function add_settings_field
+	 *
+	 * This function will contain the markup for the input feilds that we can add.
+	 *
+	 * @since x.x.x
+	 * @return void
+	 */
+	public function hfe_compatibility_option_callback() {
+		$hfe_radio_button = get_option( 'hfe_compatibility_option', '1' );
+			wp_enqueue_style( 'hfe-admin-style', HFE_URL . 'admin/assets/css/ehf-admin.css', [], HFE_VER );
+		?>
+
+		<label>
+			<input type="radio" name="hfe_compatibility_option" value= 1 <?php checked( $hfe_radio_button, 1 ); ?> > <div class="hfe_radio_options"><?php esc_html_e( ' Method 1 (Recommended)', 'header-footer-elementor' ); ?></div>
+			<p class="description"><?php esc_html_e( 'This method replaces your theme\'s header (header.php) & footer (footer.php) template with plugin\'s custom templates.', 'header-footer-elementor' ); ?></p><br>
+		</label>
+		<label>
+			<input type="radio" name="hfe_compatibility_option" value= 2 <?php checked( $hfe_radio_button, 2 ); ?> > <div class="hfe_radio_options"><?php esc_html_e( 'Method 2', 'header-footer-elementor' ); ?></div>
+			<p class="description">
+				<?php
+				echo sprintf(
+					esc_html( "This method hides your theme's header & footer template with CSS and displays custom templates from the plugin.", 'header-footer-elementor' ),
+					'<br>'
+				);
+				?>
+			</p><br>
+		</label>
+		<p class="description">
+			<?php
+			echo sprintf(
+				_e( 'Sometimes above methods might not work well with your theme, in this case, contact your theme author and request them to add support for the <a href="https://github.com/Nikschavan/header-footer-elementor/wiki/Adding-Header-Footer-Elementor-support-for-your-theme">plugin.</>', 'header-footer-elementor' ),
+				'<br>'
+			);
+			?>
+		</p>
+
+		<?php
+	}
+
+	/**
+	 * Setup Theme Support.
+	 *
+	 * @since x.x.x
+	 * @return void
+	 */
+	public function setup_fallback_support() {
+		$hfe_compatibility_option = get_option( 'hfe_compatibility_option', '1' );
+
+		if ( '1' === $hfe_compatibility_option ) {
+			require HFE_DIR . 'themes/default/class-hfe-default-compat.php';
+		} elseif ( '2' === $hfe_compatibility_option ) {
+			require HFE_DIR . 'themes/default/class-global-theme-compatibility.php';
+		}
+	}
+
+	/**
+	 * Show a settings page incase of unsupported theme.
+	 *
+	 * @since x.x.x
+	 *
+	 * @return void
+	 */
+	public function hfe_register_settings_page() {
+		add_submenu_page(
+			'themes.php',
+			__( 'Settings', 'header-footer-elementor' ),
+			__( 'Settings', 'header-footer-elementor' ),
+			'manage_options',
+			'hfe-settings',
+			[ $this, 'hfe_settings_page' ]
+		);
+	}
+
+	/**
+	 * Settings page.
+	 *
+	 * Call back function for add submenu page function.
+	 *
+	 * @since x.x.x
+	 */
+	public function hfe_settings_page() {
+		echo '<h1 class="hfe-heading-inline">';
+		esc_attr_e( 'Elementor - Header, Footer & Blocks ', 'header-footer-elementor' );
+		echo '</h1>';
+
+		?>
+		<h2 class="nav-tab-wrapper">
+			<?php
+			$tabs       = [
+				'hfe_templates' => [
+					'name' => __( 'All Templates', 'header-footer-elementor' ),
+					'url'  => admin_url( 'edit.php?post_type=elementor-hf' ),
+				],
+				'hfe_settings'  => [
+					'name' => __( 'Theme Support', 'header-footer-elementor' ),
+					'url'  => admin_url( 'themes.php?page=hfe-settings' ),
+				],
+				'hfe_guide'  => [
+					'name' => __( 'Step-By-Step Guide', 'header-footer-elementor' ),
+					'url'  => admin_url( 'themes.php?page=hfe-guide' ),
+				],
+				'hfe_about'  => [
+					'name' => __( 'About Us', 'header-footer-elementor' ),
+					'url'  => admin_url( 'themes.php?page=hfe-abou' ),
+				],
+			];
+			$active_tab = 'hfe-settings' == isset( $_GET['page'] ) && $_GET['page'] ? 'hfe_settings' : 'hfe_templates';
+			foreach ( $tabs as $tab_id => $tab ) {
+				$active = $active_tab == $tab_id ? ' nav-tab-active' : '';
+				echo '<a href="' . esc_url( $tab['url'] ) . '" class="nav-tab' . $active . '">';
+				echo esc_html( $tab['name'] );
+				echo '</a>';
+			}
+			?>
+		</h2>
+		<br />
+		<?php
+		$hfe_radio_button = get_option( 'hfe_compatibility_option', '1' );
+		?>
+		<form action="options.php" method="post">
+			<?php settings_fields( 'hfe-plugin-options' ); ?>
+			<?php do_settings_sections( 'Settings' ); ?>
+			<?php submit_button(); ?>
+		</form></div>
+		<?php
+	}
+
+	/**
+	 * Function for adding tabs
+	 *
+	 * @since x.x.x
+	 * @return void
+	 */
+	public function hfe_tabs() {
+		?>
+		<h2 class="nav-tab-wrapper">
+			<?php
+			$tabs       = [
+				'hfe_templates' => [
+					'name' => __( 'All templates', 'header-footer-elementor' ),
+					'url'  => admin_url( 'edit.php?post_type=elementor-hf' ),
+				],
+				'hfe_settings'  => [
+					'name' => __( 'Theme Support', 'header-footer-elementor' ),
+					'url'  => admin_url( 'themes.php?page=hfe-settings' ),
+				],
+				'hfe_guide'  => [
+					'name' => __( 'Step-By-Step Guide', 'header-footer-elementor' ),
+					'url'  => admin_url( 'themes.php?page=hfe-guide' ),
+				],
+				'hfe_about'  => [
+					'name' => __( 'About Us', 'header-footer-elementor' ),
+					'url'  => admin_url( 'themes.php?page=hfe-abou' ),
+				],
+			];
+			$active_tab = 'hfe-settings' == isset( $_GET['page'] ) && $_GET['page'] ? 'hfe_settings' : 'hfe_templates';
+			foreach ( $tabs as $tab_id => $tab ) {
+				$active = $active_tab == $tab_id ? ' nav-tab-active' : '';
+
+				echo '<a href="' . esc_url( $tab['url'] ) . '" class="nav-tab' . esc_attr( $active ) . '">';
+				echo esc_html( $tab['name'] );
+				echo '</a>';
+			}
+
+			?>
+		</h2>
+		<br />
+		<?php
+	}
+
+}
+
+new HFE_Settings_Page();
