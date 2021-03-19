9d40b94c755aca0ee253d1ce76d9330b0689bb5c
Sushma Kure <sushmak@bsf.io>
2021-03-19 18:29:13 +0530

    Feat: Changed popup slug

--- admin/class-addons-actions.php
+++ admin/class-addons-actions.php
@@ -15,7 +15,7 @@ function hfe_admin_modal() {
 	// Run a security check.
 	check_ajax_referer( 'hfe-admin-nonce', 'nonce' );
 
-	update_user_meta( get_current_user_id(), 'hellosush', 'dismissed' );
+	update_user_meta( get_current_user_id(), 'hfe-popup', 'dismissed' );
 }
 add_action( 'wp_ajax_hfe_admin_modal', 'hfe_admin_modal' );
 
