382f93e6005f4846f327b1ebd6ae8d5015400062
sushmak02 <sushmak@bsf.io>
2021-04-12 18:02:30 +0530

    Changed video box height and width

--- inc/class-hfe-settings-page.php
+++ inc/class-hfe-settings-page.php
@@ -361,7 +361,8 @@ class HFE_Settings_Page {
 		$is_subscribed  = get_user_meta( get_current_user_ID(), 'hfe-subscribed' );
 		$subscribe_flag = ( 'yes' === $is_subscribed[0] ) ? ' hfe-user-subscribed' : '';
 
-		$video_height = ( 'yes' === $is_subscribed[0] ) ? '320' : '300';
+		$video_height = ( 'yes' === $is_subscribed[0] ) ? '380' : '300';
+		$video_width = ( 'yes' === $is_subscribed[0] ) ? '650' : '560';
 		?>
 
 		<div class="hfe-admin-about-section hfe-admin-columns hfe-admin-guide-section<?php echo $subscribe_flag; ?>">
@@ -372,7 +373,7 @@ class HFE_Settings_Page {
 					<?php if ( 'page' === $type ) { ?>
 						<h3><?php esc_html_e( 'Learn the Art of Designing Custom Header & Footer', 'header-footer-elementor' ); ?></h3>
 						<div class="hfe-admin-video">
-							<iframe width="560" height=<?php echo $video_height; ?> src="https://www.youtube.com/embed/XLEQb2hF2Fo?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							<iframe width=<?php echo $video_width; ?> height=<?php echo $video_height; ?> src="https://www.youtube.com/embed/XLEQb2hF2Fo?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</div>
 					<?php } elseif ( 'popup' === $type ) { ?>
 						<h2><?php esc_html_e( 'Get Creative Header & Footer Examples', 'header-footer-elementor' ); ?></h2>
