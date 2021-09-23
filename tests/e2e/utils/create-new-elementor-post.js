9a35ed58e349044d0d9b12775d504480608cd2b5
Nikhil <email@nikhilchavan.com>
2021-09-23 16:44:23 +0530

    Introduce a helper function `createNewElementorPost()`

---
+++ tests/e2e/utils/create-new-elementor-post.js
@@ -0,0 +1,24 @@
+import { createNewPost } from '@wordpress/e2e-test-utils';
+
+export async function createNewElementorPost(
+    postType,
+	title,
+	content,
+	excerpt,
+) {
+    await createNewPost({
+        postType,
+        title,
+        content,
+        excerpt,
+    });
+
+    // Wait for the editor to be ready.
+    await page.waitForSelector( '#elementor-switch-mode-button' );
+
+    // Click on `Edit With Elementor` button
+    const editWithElementorButton = await page.$( '#elementor-switch-mode-button' );
+    editWithElementorButton.click();
+
+    await page.waitForSelector( '#elementor-panel-saver-button-publish' );
+}
