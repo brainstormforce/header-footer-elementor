3777f016f978ba213e0605ae0b646a80171f2d0c
Nikhil <email@nikhilchavan.com>
2021-09-23 23:28:31 +0530

    cs fixes

--- tests/e2e/utils/publish-page.js
+++ tests/e2e/utils/publish-page.js
@@ -1,5 +1,7 @@
 export async function publishPage() {
-	const publishButton = await page.$( '#elementor-panel-saver-button-publish' );
+	const publishButton = await page.$(
+		'#elementor-panel-saver-button-publish',
+	);
 	publishButton.evaluate( ( b ) => b.click() );
 
 	await page.waitForSelector( '#elementor-toast' );
--- tests/e2e/utils/view-page.js
+++ tests/e2e/utils/view-page.js
@@ -2,6 +2,8 @@ export async function viewPage() {
 	const elementorMenu = await page.$( '#elementor-panel-header-menu-button' );
 	elementorMenu.evaluate( ( b ) => b.click() );
 
-	const viewPageButton = await page.$( '.elementor-panel-menu-item-view-page a' );
+	const viewPageButton = await page.$(
+		'.elementor-panel-menu-item-view-page a',
+	);
 	viewPageButton.evaluate( ( b ) => b.click() );
 }
