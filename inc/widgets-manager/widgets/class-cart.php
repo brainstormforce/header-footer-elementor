3be51e3ccb4e7926df4112be55e1a319569592a7
Vijeta Raikar <110518282+vijetaR@users.noreply.github.com>
2023-10-15 23:34:33 +0530

    missing span tag

--- inc/widgets-manager/widgets/class-cart.php
+++ inc/widgets-manager/widgets/class-cart.php
@@ -631,6 +631,7 @@ class Cart extends Widget_Base {
 							<?php if ( null !== WC()->cart ) { ?>
 								<span class="hfe-cart-count">
 									<?php echo esc_html( WC()->cart->get_cart_contents_count() ); ?>
+							</span>
 							<?php } ?>
 						</div>
 					</a>
