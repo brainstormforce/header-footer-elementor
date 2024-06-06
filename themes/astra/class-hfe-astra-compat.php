a294ae108d895afbb751219812ce5218cb03f803
Harish Dhanraj Sugandhi <68777106+harishsganndhi123@users.noreply.github.com>
2024-06-06 09:47:44 +0530

    Resolved PHPStan and PHPCS errors from themes/astra/class-hfe-astra-compat.php

--- themes/astra/class-hfe-astra-compat.php
+++ themes/astra/class-hfe-astra-compat.php
@@ -19,12 +19,14 @@ class HFE_Astra_Compat {
 
 	/**
 	 *  Initiator
+	 *
+	 * @return HFE_Astra_Compat
 	 */
 	public static function instance() {
 		if ( ! isset( self::$instance ) ) {
 			self::$instance = new HFE_Astra_Compat();
 
-			add_action( 'wp', [ self::$instance, 'hooks' ] );
+			add_action( 'wp', array( self::$instance, 'hooks' ) );
 		}
 
 		return self::$instance;
@@ -32,15 +34,17 @@ class HFE_Astra_Compat {
 
 	/**
 	 * Run all the Actions / Filters.
+	 *
+	 * @return void
 	 */
 	public function hooks() {
 		if ( hfe_header_enabled() ) {
-			add_action( 'template_redirect', [ $this, 'astra_setup_header' ], 10 );
+			add_action( 'template_redirect', array( $this, 'astra_setup_header' ), 10 );
 			add_action( 'astra_header', 'hfe_render_header' );
 		}
 
 		if ( hfe_footer_enabled() ) {
-			add_action( 'template_redirect', [ $this, 'astra_setup_footer' ], 10 );
+			add_action( 'template_redirect', array( $this, 'astra_setup_footer' ), 10 );
 			add_action( 'astra_footer', 'hfe_render_footer' );
 		}
 
@@ -51,25 +55,29 @@ class HFE_Astra_Compat {
 
 	/**
 	 * Disable header from the theme.
+	 *
+	 * @return void
 	 */
 	public function astra_setup_header() {
 		remove_action( 'astra_header', 'astra_header_markup' );
 
 		// Remove the new header builder action.
 		if ( class_exists( 'Astra_Builder_Helper' ) && Astra_Builder_Helper::$is_header_footer_builder_active ) {
-			remove_action( 'astra_header', [ Astra_Builder_Header::get_instance(), 'prepare_header_builder_markup' ] );
+			remove_action( 'astra_header', array( Astra_Builder_Header::get_instance(), 'prepare_header_builder_markup' ) );
 		}
 	}
 
 	/**
 	 * Disable footer from the theme.
+	 *
+	 * @return void
 	 */
 	public function astra_setup_footer() {
 		remove_action( 'astra_footer', 'astra_footer_markup' );
 
 		// Remove the new footer builder action.
 		if ( class_exists( 'Astra_Builder_Helper' ) && Astra_Builder_Helper::$is_header_footer_builder_active ) {
-			remove_action( 'astra_footer', [ Astra_Builder_Footer::get_instance(), 'footer_markup' ] );
+			remove_action( 'astra_footer', array( Astra_Builder_Footer::get_instance(), 'footer_markup' ) );
 		}
 	}
 }
