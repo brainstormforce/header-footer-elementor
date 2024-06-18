dd9955d99033a4bd70edc9797d8a7068d37c47ae
Harish Dhanraj Sugandhi <68777106+harishsganndhi123@users.noreply.github.com>
2024-06-18 18:54:52 +0530

    Resolved errors using phpcbf

--- themes/default/class-hfe-default-compat.php
+++ themes/default/class-hfe-default-compat.php
@@ -16,7 +16,7 @@ class HFE_Default_Compat {
 	 *  Initiator
 	 */
 	public function __construct() {
-		add_action( 'wp', [ $this, 'hooks' ] );
+		add_action( 'wp', array( $this, 'hooks' ) );
 	}
 
 	/**
@@ -27,7 +27,7 @@ class HFE_Default_Compat {
 	public function hooks(): void {
 		if ( hfe_header_enabled() ) {
 			// Replace header.php template.
-			add_action( 'get_header', [ $this, 'override_header' ] );
+			add_action( 'get_header', array( $this, 'override_header' ) );
 
 			// Display HFE's header in the replaced header.
 			add_action( 'hfe_header', 'hfe_render_header' );
@@ -35,7 +35,7 @@ class HFE_Default_Compat {
 
 		if ( hfe_footer_enabled() || hfe_is_before_footer_enabled() ) {
 			// Replace footer.php template.
-			add_action( 'get_footer', [ $this, 'override_footer' ] );
+			add_action( 'get_footer', array( $this, 'override_footer' ) );
 		}
 
 		if ( hfe_footer_enabled() ) {
@@ -44,7 +44,7 @@ class HFE_Default_Compat {
 		}
 
 		if ( hfe_is_before_footer_enabled() ) {
-			add_action( 'hfe_footer_before', [ 'Header_Footer_Elementor', 'get_before_footer_content' ] );
+			add_action( 'hfe_footer_before', array( 'Header_Footer_Elementor', 'get_before_footer_content' ) );
 		}
 	}
 
@@ -57,7 +57,7 @@ class HFE_Default_Compat {
 	 */
 	public function override_header(): void {
 		require HFE_DIR . 'themes/default/hfe-header.php';
-		$templates   = [];
+		$templates   = array();
 		$templates[] = 'header.php';
 		// Avoid running wp_head hooks again.
 		remove_all_actions( 'wp_head' );
@@ -75,7 +75,7 @@ class HFE_Default_Compat {
 	 */
 	public function override_footer(): void {
 		require HFE_DIR . 'themes/default/hfe-footer.php';
-		$templates   = [];
+		$templates   = array();
 		$templates[] = 'footer.php';
 		// Avoid running wp_footer hooks again.
 		remove_all_actions( 'wp_footer' );
@@ -83,7 +83,6 @@ class HFE_Default_Compat {
 		locate_template( $templates, true );
 		ob_get_clean();
 	}
-
 }
 
 new HFE_Default_Compat();
