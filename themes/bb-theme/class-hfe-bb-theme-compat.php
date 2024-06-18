a1ee866a8c2eb2df48fb31400453462ef7d7c40b
Harish Dhanraj Sugandhi <68777106+harishsganndhi123@users.noreply.github.com>
2024-06-18 18:54:10 +0530

    Resolved errors using phpcbf

--- themes/bb-theme/class-hfe-bb-theme-compat.php
+++ themes/bb-theme/class-hfe-bb-theme-compat.php
@@ -12,88 +12,87 @@
  */
 class HFE_BB_Theme_Compat {
 
-    /**
-     * Instance of HFE_BB_Theme_Compat
-     *
-     * @var HFE_BB_Theme_Compat|null
-     */
-    private static $instance = null;
+	/**
+	 * Instance of HFE_BB_Theme_Compat
+	 *
+	 * @var HFE_BB_Theme_Compat|null
+	 */
+	private static $instance = null;
 
-    /**
-     *  Initiator
-     *
-     * @return self
-     */
-    public static function instance(): self {
-        if ( ! isset( self::$instance ) ) {
-            self::$instance = new self();
+	/**
+	 *  Initiator
+	 *
+	 * @return self
+	 */
+	public static function instance(): self {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self();
 
-            add_action( 'wp', [ self::$instance, 'hooks' ] );
-        }
+			add_action( 'wp', array( self::$instance, 'hooks' ) );
+		}
 
-        return self::$instance;
-    }
+		return self::$instance;
+	}
 
-    /**
-     * Run all the Actions / Filters.
-     *
-     * @return void
-     */
-    public function hooks(): void {
-        if ( hfe_header_enabled() ) {
-            add_filter( 'fl_header_enabled', '__return_false' );
-            add_action( 'fl_before_header', [ $this, 'get_header_content' ] );
-        }
+	/**
+	 * Run all the Actions / Filters.
+	 *
+	 * @return void
+	 */
+	public function hooks(): void {
+		if ( hfe_header_enabled() ) {
+			add_filter( 'fl_header_enabled', '__return_false' );
+			add_action( 'fl_before_header', array( $this, 'get_header_content' ) );
+		}
 
-        if ( hfe_is_before_footer_enabled() ) {
-            add_action( 'fl_after_content', [ 'Header_Footer_Elementor', 'get_before_footer_content' ], 10 );
-        }
+		if ( hfe_is_before_footer_enabled() ) {
+			add_action( 'fl_after_content', array( 'Header_Footer_Elementor', 'get_before_footer_content' ), 10 );
+		}
 
-        if ( hfe_footer_enabled() ) {
-            add_filter( 'fl_footer_enabled', '__return_false' );
-            add_action( 'fl_after_content', [ $this, 'get_footer_content' ] );
-        }
-    }
+		if ( hfe_footer_enabled() ) {
+			add_filter( 'fl_footer_enabled', '__return_false' );
+			add_action( 'fl_after_content', array( $this, 'get_footer_content' ) );
+		}
+	}
 
-    /**
-     * Display header markup for beaver builder theme.
-     *
-     * @return void
-     */
-    public function get_header_content(): void {
-        $header_layout = FLTheme::get_setting( 'fl-header-layout' );
+	/**
+	 * Display header markup for beaver builder theme.
+	 *
+	 * @return void
+	 */
+	public function get_header_content(): void {
+		$header_layout = FLTheme::get_setting( 'fl-header-layout' );
 
-        if ( 'none' == $header_layout || is_page_template( 'tpl-no-header-footer.php' ) ) {
-            return;
-        }
+		if ( 'none' == $header_layout || is_page_template( 'tpl-no-header-footer.php' ) ) {
+			return;
+		}
 
-        ?>
-        <header id="masthead" itemscope="itemscope" itemtype="https://schema.org/WPHeader">
-            <p class="main-title bhf-hidden" itemprop="headline"><a href="<?php echo bloginfo( 'url' ); ?>"
-                                                                    title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
-                                                                    rel="home"><?php bloginfo( 'name' ); ?></a></p>
-            <?php Header_Footer_Elementor::get_header_content(); ?>
-        </header>
-        <?php
-    }
+		?>
+		<header id="masthead" itemscope="itemscope" itemtype="https://schema.org/WPHeader">
+			<p class="main-title bhf-hidden" itemprop="headline"><a href="<?php echo bloginfo( 'url' ); ?>"
+																	title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+																	rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php Header_Footer_Elementor::get_header_content(); ?>
+		</header>
+		<?php
+	}
 
-    /**
-     * Display footer markup for beaver builder theme.
-     *
-     * @return void
-     */
-    public function get_footer_content(): void {
-        if ( is_page_template( 'tpl-no-header-footer.php' ) ) {
-            return;
-        }
+	/**
+	 * Display footer markup for beaver builder theme.
+	 *
+	 * @return void
+	 */
+	public function get_footer_content(): void {
+		if ( is_page_template( 'tpl-no-header-footer.php' ) ) {
+			return;
+		}
 
-        ?>
-        <footer itemscope="itemscope" itemtype="https://schema.org/WPFooter">
-            <?php Header_Footer_Elementor::get_footer_content(); ?>
-        </footer>
-        <?php
-    }
-
+		?>
+		<footer itemscope="itemscope" itemtype="https://schema.org/WPFooter">
+			<?php Header_Footer_Elementor::get_footer_content(); ?>
+		</footer>
+		<?php
+	}
 }
 
 HFE_BB_Theme_Compat::instance();
