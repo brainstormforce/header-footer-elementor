6afadfef0ef789d1ee7fa259c60df836d5343d4d
Sushma Kure <sushmak@bsf.io>
2024-10-08 11:18:10 +0530

    Update with main branch

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'c3b4cf20bbcbc38aac1c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '6ac1e43469c52a6f2685');
