5657423a1f77c42781890eeb29b4c13c3db79675
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2024-10-15 09:04:37 +0530

    added templates screen

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '2a5db00e4a9fb8f55ea3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '6036657fe6959cb29bcf');
---
+++ src/Components/Templates/ExploreTemplates.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { Container, Title, Button } from "@bsf/force-ui";
+import { Zap, Plus, Check } from "lucide-react";
+
+const ExploreTemplates = () => {
+
+    const templateData = [
+		{
+			id: 1,
+			icon: "",
+			title: "250+ templates for evrry niche",
+		},
+		{
+			id: 2,
+			icon: "",
+			title: "Modern, timeless designs",
+		},
+		{
+			id: 3,
+			icon: "",
+			title: "Full design flexibility for easy customization",
+		},
+		{
+			id: 4,
+			icon: "",
+			title: "100% responsive across all devices",
+		}
+	];
+
+  return (
+    <div>
+			<Container
+				className="bg-background-primary p-4 border-[0.5px] border-subtle rounded-xl shadow-sm"
+				containerType="flex"
+				direction="row"
+				justify="between"
+				gap="xs"
+			>
+				<Container.Item className="flex flex-col justify-between" style={{width:"40%"}}>
+					<div>
+						<Title
+							description=""
+							icon={<Zap />}
+							iconPosition="left"
+							size="xs"
+							tag="h6"
+							title="Design Your Website in Minutes"
+							className="text-xs font-semibold text-brand-primary-600"
+						/>
+						<Title
+							description=""
+							icon={""}
+							iconPosition="left"
+							tag="h6"
+							title="Build your website faster using our prebuilt templates"
+							className="py-1 text-sm"
+						/>
+						<p className="text-md m-0 text-text-secondary">
+                        Stop building your site from scratch. Use our professional templates for your stunning website.
+                        It’s easy to customize and completely responsive. Explore hundreds of designs and bring your vision to life in no time.
+						</p>
+					</div>
+					<div className="grid grid-cols-1 grid-flow-row gap-1 my-4">
+						{templateData.map((template) => (
+							<Title
+							key={template.id}
+								description=""
+								icon={
+									<Check className="text-brand-primary-600 mr-1 h-3 w-3" />
+								}
+								iconPosition="left"
+								size="xs"
+								tag="h6"
+								title={template.title}
+								className=""
+							/>
+						))}
+					</div>
+					<div className="flex items-center pb-3 gap-4">
+						<Button
+							icon={<Plus />}
+							iconPosition="right"
+							variant="secondary"
+                            style={{backgroundColor:"#6005FF"}}
+							className=""
+						>
+							Explore Templates
+						</Button>
+						<Button icon={""} iconPosition="right" variant="ghost">
+							Learn More
+						</Button>
+					</div>
+				</Container.Item>
+				<Container.Item className="flex justify-center items-center" style={{width:"60%"}}>
+					<img
+						src={`${hfeSettingsData.template_url}`}
+						alt="Column Showcase"
+						className="w-full h-auto rounded"
+					/>
+				</Container.Item>
+			</Container>
+		</div>
+  )
+}
+
+export default ExploreTemplates
