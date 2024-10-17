fe25f66818531f7465749c8f4818f8a3d7e3aba9
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2024-10-17 11:25:37 +0530

    added widgets header

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'f3ef6b678f140a7f8df2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '625b29d298c7a34894b2');
--- src/Components/Widgets/Features/FeatureWidgets.jsx
+++ src/Components/Widgets/Features/FeatureWidgets.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Container } from "@bsf/force-ui";
-import { MoreHorizontalIcon, Plus, Map } from "lucide-react";
+import { Container, Button } from "@bsf/force-ui";
+import { MoreHorizontalIcon, Plus, Map, House, SearchIcon } from "lucide-react";
 import FeatureWidgetItems from './FeatureWidgetItems';
 
 const widgetData = [
@@ -502,41 +502,64 @@ const widgetData = [
 ];
 
 const FeatureWidgets = () => {
-  return (
-    <div className='rounded-lg bg-white w-full mb-4'>
-    <div className='flex items-center justify-between' style={{
-        paddingTop: '12px',
-        paddingInline: '16px'
-    }}>
-        <p className='m-0 text-sm font-semibold text-text-primary'>Widgets / Features</p>
-        <div className='flex items-center gap-x-2 mr-7'>
-            <p className='m-0 text-xs font-semibold text-text-primary'>View all</p>
-            <MoreHorizontalIcon />
-        </div>
-    </div>
-    <div className='flex bg-black flex-col rounded-lg p-4'>
+    return (
+        <div className='rounded-lg bg-white w-full mb-4'>
+            <div className='flex items-center justify-between' style={{
+                paddingTop: '12px',
+                paddingInline: '16px'
+            }}>
+                <p className='m-0 text-sm font-semibold text-text-primary'>Widgets / Features</p>
+                <div className='flex items-center gap-x-2 mr-7'>
+                    <SearchIcon
+                        className="absolute pl-2 left-1/2 top-1/2"
+                    />
+                    <input
+                        type="search"
+                        placeholder="Search..."
+                        icon={<Plus />}
+                        className="mr-2 pl-10"
+                        style={{ height: '40px', backgroundColor: '#F9FAFB', }}
+                    />
+                    <Button
+                        iconPosition="left"
+                        variant="outline"
+                    >
+                        Activate all
+                    </Button>
 
-        <Container
-            align="stretch"
-            className="bg-background-gray p-1 gap-1.5"
-            cols={4}
-            containerType="grid"
-            gap=""
-            justify="start"
-        >
-            {widgetData.map((widget) => (
-                <Container.Item
-                key={widget.id}
-                    alignSelf="auto"
-                    className="text-wrap rounded-md shadow-container-item bg-background-primary p-4"
+                    <Button
+                        iconPosition="left"
+                        variant="outline"
+                    >
+                        Deactivate all
+                    </Button>
+
+                    <MoreHorizontalIcon />
+                </div>
+            </div>
+            <div className='flex bg-black flex-col rounded-lg p-4'>
+
+                <Container
+                    align="stretch"
+                    className="bg-background-gray p-1 gap-1.5"
+                    cols={4}
+                    containerType="grid"
+                    gap=""
+                    justify="start"
                 >
-                    <FeatureWidgetItems widget={widget} key={widget.id} />
-                </Container.Item>
-            ))}
-        </Container>
-    </div>
-</div>
-  )
+                    {widgetData.map((widget) => (
+                        <Container.Item
+                            key={widget.id}
+                            alignSelf="auto"
+                            className="text-wrap rounded-md shadow-container-item bg-background-primary p-4"
+                        >
+                            <FeatureWidgetItems widget={widget} key={widget.id} />
+                        </Container.Item>
+                    ))}
+                </Container>
+            </div>
+        </div>
+    )
 }
 
 export default FeatureWidgets
