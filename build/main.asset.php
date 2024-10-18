e08b9b84856376578cc29b0f95b210096c37b8b4
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2024-10-18 14:57:37 +0530

    added spinner for dashboard page

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ddc9d14791c87c2fb81e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1ebd1321b838466ea4ae');
--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import { useState, useEffect } from 'react';
-import './styles.css';
+import { useState, useEffect } from "react";
 import "@fontsource/figtree"; // Defaults to weight 400
 import "@fontsource/figtree/400.css"; // Specify weight
-import "@fontsource/figtree/400-italic.css"; // Specify weight and style 
-import CustomRouter from 'router/customRouter';
+import "@fontsource/figtree/400-italic.css"; // Specify weight and style
+import CustomRouter from "router/customRouter";
+import { LoaderPinwheel } from "lucide-react";
+import { Loader } from "@bsf/force-ui";
 
 const App = () => {
-  const [loaded, setLoaded] = useState(false);
+    const [loaded, setLoaded] = useState(false);
 
-  // scroll top on route change
-  window.onhashchange = () => {
-    window.scrollTo(0, 0);
-  };
+    // scroll top on route change
+    window.onhashchange = () => {
+        window.scrollTo(0, 0);
+    };
 
-  // Simulate loading (replace with actual loading logic if needed)
-  useEffect(() => {
-    setTimeout(() => {
-      setLoaded(true);
-    }, 1000); // Simulating a load delay of 1 second
-  }, []);
+    // Simulate loading (replace with actual loading logic if needed)
+    useEffect(() => {
+        setTimeout(() => {
+            setLoaded(true);
+        }, 1000); // Simulating a load delay of 1 second
+    }, []);
 
-  if (!loaded) {
-    return <div className="loading-spinner">Loading...</div>;
-  }
+    if (!loaded) {
+        return (
+            <div className="loading-spinner flex items-center justify-center h-screen">
+                <Loader
+                    icon={<LoaderPinwheel className="animate-spin" />}
+                    size="md"
+                    variant="primary"
+                    className="flex items-center justify-center"
+                    style={{ background: 'none' }}
+                />
+            </div>
+        );
+    }
 
-  return (
-    <div className="app-container font-figtree">
-      <CustomRouter />
-    </div>
-  );
+    return (
+        <div className="app-container font-figtree">
+            <CustomRouter />
+        </div>
+    );
 };
 
 export default App;
