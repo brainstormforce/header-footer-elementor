fc3b2880a9dae8e2109a5d353fdeed7a659d9537
Sushma Kure <sushmak@bsf.io>
2024-10-18 18:14:24 +0530

    Added search widget functionality

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6da84e1c8aeb416f50a4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b48ea8c0e77b08be50d6');
