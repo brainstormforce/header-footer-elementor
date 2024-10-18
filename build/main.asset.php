5b353684de8b12122295b73603e391df25092bd5
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2024-10-18 18:51:57 +0530

    updated pr

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b48ea8c0e77b08be50d6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4a9da80c25a173dafcf5');
