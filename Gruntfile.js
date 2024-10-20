6cf206682cb4b68985f5dae8270c282b29b63442
Vijeta Raikar <110518282+vijetaR@users.noreply.github.com>
2024-10-20 21:05:12 +0530

    gruntfile rule added

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,7 +102,19 @@ module.exports = function( grunt ) {
 					}
 				]
 			}
+		},
+
+		copy: {
+			main: {
+				options: {
+					mode: true
+				},
+				src: [
+					'!phpinsights.php',
+				],
+				dest: 'header-footer-elementor/'
 		}
+		},
 
 	} );
 	
