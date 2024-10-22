06b62da2cbab9b98bae2b5215824f0407945561f
Sushma Kure <sushmak@bsf.io>
2024-10-22 22:08:55 +0530

    Fixed plugin activation and install issue

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '63ed1323df19867da18e');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '59abc36ea625c961935d');
