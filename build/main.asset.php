da377968bae550c962662cdc4e4094d788eeb317
Sushma Kure <sushmak@bsf.io>
2024-10-22 15:09:54 +0530

    Hide Pro Badge for pro widgets

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '39c1e7fe3dab93e3e784');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c6cde8939b84c2c18faf');
