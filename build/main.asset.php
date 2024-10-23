7653fcbe6d99819b02b0f77a7066c3ad0b17b66b
Sushma Kure <sushmak@bsf.io>
2024-10-23 12:57:45 +0530

    Template screen style fixes

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '59abc36ea625c961935d');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'cd246fe25a023e33bcff');
