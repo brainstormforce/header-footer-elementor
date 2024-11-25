ee869041a96730ec2310cbbf57b4577870123776
Sushma Kure <sushmak@bsf.io>
2024-11-25 17:07:54 +0530

    Comment Pro code for testing purpose

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c34fe9b793fbd3217f37');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e09b746267d268117cce');
