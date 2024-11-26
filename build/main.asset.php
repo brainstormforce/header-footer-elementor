b90042749ff65c2f23f94b04e07cd118b1530063
Sushma Kure <sushmak@bsf.io>
2024-11-26 22:35:03 +0530

    Fixed theme support issue

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd1bad0359e4ecca379fd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9eb5608d2e3462976ccd');
