e0b38c65df3688de9509d8405cf6ba75a08847e6
Sushma Kure <sushmak@bsf.io>
2024-11-26 14:44:53 +0530

    Fixed nav menu issues

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '732f95ad52f8073f4824');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5be555031d0999470b8d');
