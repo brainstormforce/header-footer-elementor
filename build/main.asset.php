92d7b0d819545d4bdaafde05762dcb20a5542ffa
Sushma Kure <sushmak@bsf.io>
2024-11-26 17:17:00 +0530

    Grunt

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '30504e4b5ce5d41aebd4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '62741bd5dcb7b2591c89');
