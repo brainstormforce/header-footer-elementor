f5053d75b6675de06ee7a413e6e13a99d53953a0
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2024-11-28 08:13:51 +0530

    fixed topbar styles

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'df279c5ed61aeb3eebb3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '49f128e196163b2ab40f');
