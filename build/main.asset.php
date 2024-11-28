74b44e7740e9c228f2a9d56ec03689f0a8834ba8
Sushma Kure <sushmak@bsf.io>
2024-11-28 15:39:10 +0530

    Added link for button

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '391ac20695b473352e5b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '24c8624962fe4409a559');
