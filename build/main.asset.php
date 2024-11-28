e9cdeef3257e73efa8617da03d3455d84874a3b0
Sushma Kure <sushmak@bsf.io>
2024-11-28 13:48:21 +0530

    Hide demo and doc links when not available

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9640349de0030d6ee5f2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '808be3407a73fcdef09c');
