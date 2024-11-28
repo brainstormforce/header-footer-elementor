bf1248cb2ae3965c55d0b307e45ca0e4d5007e20
Sushma Kure <sushmak@bsf.io>
2024-11-28 13:56:35 +0530

    FInal fixes done

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '808be3407a73fcdef09c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2516320c1724ccc6cac9');
