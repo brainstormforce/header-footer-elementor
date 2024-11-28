8eb8f1dae5438a6330d050d2dc4c837facc0b239
Sushma Kure <sushmak@bsf.io>
2024-11-28 14:28:42 +0530

    revert changes

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '40063891f4bd83d4705f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8330a728b79e8db885a7');
