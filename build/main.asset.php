d3f0eb3282f9ad7675b1bbedb81f419f1bbec621
Sushma Kure <sushmak@bsf.io>
2024-12-02 16:53:44 +0530

    run build

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '668ac494e4ebca80cfb9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ee5733925c52454df14b');
