292e6de7b5fb384479b2b6a3841e696e71209fca
Vijeta Raikar <110518282+vijetaR@users.noreply.github.com>
2024-12-02 12:30:46 +0530

    Updated Menu name error

--- admin/class-hfe-admin.php
+++ admin/class-hfe-admin.php
@@ -298,7 +298,7 @@ class HFE_Admin {
 		add_submenu_page(
 			$setting_location,
 			__( 'Header/Footer Builder', 'header-footer-elementor' ),
-			__( 'Header/Footer Builder', 'header-footer-elementor' ),
+			__( 'Header Footer Builder', 'header-footer-elementor' ),
 			'edit_pages',
 			'edit.php?post_type=elementor-hf',
 			'',
--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '543daab2cede882064dd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4728418c9c66c2586476');
