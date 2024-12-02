45b01e9016cbd58e4b1da88517f586b24cd5f81d
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2024-12-02 15:30:57 +0530

    updated free vs pro page

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4362bd9474d3578eda28');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '17e841a937182abab693');
