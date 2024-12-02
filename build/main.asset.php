ade6be533697f7f9e2a618c8e5566c41fc6e0ad0
Sushma Kure <sushmak@bsf.io>
2024-12-02 14:11:51 +0530

    Disable new widgets by default

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4362bd9474d3578eda28');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '85e1dca09f30a405617c');
