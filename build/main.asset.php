a7b8c98f836d461269b43fbda667e04ce156fe04
Sushma Kure <sushmak@bsf.io>
2024-12-03 10:15:58 +0530

    Added hfe link in topbar

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '69b162d12f733fdc803d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'a3ea56e1a05c3dee4d0b');
