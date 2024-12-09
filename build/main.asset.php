1f03448e9622d7b5195aa3d3cc435cb2d90bc1af
Sushma Kure <sushmak@bsf.io>
2024-12-09 13:47:55 +0530

    Added topbar css

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '948a877200b28c8a97df');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c20a9ce80c27bbdd5573');
