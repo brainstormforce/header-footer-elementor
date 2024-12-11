a14409b877ed853adfbf60443633c3e9bc927b34
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2024-12-11 16:49:14 +0530

    added react topbar to hfe page

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c20a9ce80c27bbdd5573');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'a048d22cd743534c0383');
