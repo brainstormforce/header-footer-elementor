f785b77fabe1c0e3da793d9678956b1731786c36
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2024-12-19 12:28:54 +0530

    updated pr feedback

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e01ccfb9c4eeebe433d4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c0a3b617e7507b909970');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client"; // Import from react-dom/client for React 18
 import domReady from "@wordpress/dom-ready";
 import App from "./App";
-import NavMenu from '@components/NavMenu'
+import NavMenu from "@components/NavMenu";
 import "./styles.css";
 
 domReady(() => {
@@ -11,13 +11,19 @@ domReady(() => {
         root.render(<App />);
     }
 
-    if ( "yes" === hfe_admin_data.show_view_all || window.location.href === hfeSettingsData.header_footer_builder || window.location.href === hfeSettingsData.header_footer_builder || "yes" === hfeSettingsData.is_hfe_post ) {
-        const navMenuElement = document.getElementById("hfe-admin-top-bar-root");
+    if (
+        "yes" === hfe_admin_data.show_view_all ||
+        window.location.href === hfeSettingsData.header_footer_builder ||
+        "yes" === hfeSettingsData.is_hfe_post
+    ) {
+        const navMenuElement = document.getElementById(
+            "hfe-admin-top-bar-root"
+        );
         if (navMenuElement) {
             const newDiv = document.createElement("div");
             newDiv.id = "hfe-settings-app";
             navMenuElement.appendChild(newDiv);
-    
+
             const navMenuRoot = createRoot(newDiv);
             navMenuRoot.render(<NavMenu />);
         }
