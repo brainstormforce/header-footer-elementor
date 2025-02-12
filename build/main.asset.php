499648fc89d35fdc80d86232eb2636ce96cf3f65
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-02-12 16:50:17 +0530

    updated styling for build screen

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c3030fb95dbee6f9a780');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd9141fe20da94ff740b7');
--- src/Components/Onboarding/Build.jsx
+++ src/Components/Onboarding/Build.jsx
@@ -14,37 +14,39 @@ const OnboardingBuild = ({ setCurrentStep }) => {
     };
 
     return (
-        <div className="bg-background-primary border-[0.5px] border-subtle ml-10 rounded-xl shadow-sm mb-6 p-8">
-            <div className="flex items-center justify-between w-full">
+        <div className="bg-background-primary border-[0.5px] border-subtle rounded-xl shadow-sm mb-6 p-8">
+            <div className="flex items-center">
                 {/* Left Content */}
-                <div className="flex flex-col items-start w-1/2">
-                    <p className="font-bold text-text-primary m-0 mt-2" style={{ fontSize: '30px' }}>
+                <div className="flex flex-col items-start">
+                    <p className="font-bold text-text-primary m-0" style={{ fontSize: '20px' }}>
                         {__("You’re all set! 🚀", "header-footer-elementor")}
                     </p>
-                    <p className="font-medium text-text-tertiary m-0 mt-2 text-base w-[350px]">
+                    <p className="font-medium text-text-tertiary m-0 text-sm">
                         {__(
-                            "Start creating headers, footers, or pages with UAE and take your website to the next level",
+                            "Start creating amazing designs with UAE and take your website to the next level",
                             "header-footer-elementor"
                         )}
                     </p>
-                    <p className="font-bold text-text-primary m-0 mt-2 text-base">
-                        {__("Here’s What UAE Will Do for You Now:", "header-footer-elementor")}
+                    <p className="font-bold text-text-primary m-0 mt-1 text-lg" style={{ paddingTop: '8px'}}>
+                        {__("Here’s how to get started:", "header-footer-elementor")}
                     </p>
-                    <ul className="list-none pl-0 space-y-2">
-                        <li className="flex items-start gap-x-2 text-field-label text-sm font-medium">
-                            <Check className="size-4 text-[#6005ff]" />
-                            <span className="text-text-primary w-[340px]">
-                                Design the headers and footers, UAE will automatically place it in your desired location.
+                    <ul className="list-none">
+                        <li className="flex items-start gap-x-1 text-text-tertiary text-md font-medium">
+                            <span className="text-text-tertiary">
+                                1. Click on “Create New Page” button
                             </span>
                         </li>
-                        <li className="flex items-start gap-x-2 text-field-label text-sm font-medium">
-                            <Check className="size-4 text-icon-primary" />
-                            <span>Create before footer, custom blocks etc</span>
+                        <li className="flex items-start gap-x-1 text-text-tertiary text-md font-medium">
+                            <span>2. Use the Elementor editor to customize your page/post according to your preferences</span>
                         </li>
-                        <li className="flex items-start gap-x-2 text-field-label text-sm font-medium">
-                            <Check className="size-4 text-icon-primary" />
-                            <span className="text-text-primary w-[330px]">
-                                Work with any theme in your website, UAE can handle everything with ease.
+                        <li className="flex items-start gap-x-1 text-text-tertiary text-md font-medium">
+                            <span className="text-text-tertiary">
+                                3. Use UAE widgets to design your pages.
+                            </span>
+                        </li>
+                        <li className="flex items-start gap-x-1 text-text-tertiary text-md font-medium">
+                            <span className="text-text-tertiary">
+                                4. Click “Publish” to make it live
                             </span>
                         </li>
                     </ul>
@@ -54,14 +56,13 @@ const OnboardingBuild = ({ setCurrentStep }) => {
                 <div className="w-1/2 flex justify-center">
                     <img
                         alt="Build"
-                        className="w-full max-w-[400px] object-contain"
+                        className="w-full object-contain"
+                        style={{ height: '250px' }} 
                         src={`${hfeSettingsData.build_banner}`}
                     />
                 </div>
             </div>
-
-            <hr className="mt-6 w-full border-b-0 border-x-0 border-t border-solid border-t-border-subtle" />
-            <div className='flex flex-row gap-2' style={{ marginTop: '40px' }}>
+            <div className='flex flex-row gap-2' style={{ marginTop: '15px' }}>
                 <Button
                     iconPosition="right"
                     variant="primary"
@@ -121,20 +122,20 @@ const OnboardingBuild = ({ setCurrentStep }) => {
             <div
                 className="flex items-start justify-start"
                 style={{
-                    marginTop: '40px',
+                    marginTop: '25px',
                     backgroundImage: `url(${hfeSettingsData.special_reward})`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
-                    width: '100%', // Adjust width as needed
+                    width: '95%', // Adjust width as needed
                     height: '150px' // Adjust height as needed
                 }}
             >
                 <div className='flex flex-col'>
-                    <span className='font-bold text-text-primary' style={{ fontSize: '20px', marginTop: '34px', marginLeft: '34px' }}>
+                    <span className='font-bold text-text-primary' style={{ fontSize: '16px', marginTop: '34px', marginLeft: '34px' }}>
                         We have a special reward just for you!
                     </span>
 
-                    <span className='font-medium text-text-secondary' style={{ fontSize: '16px', marginTop: '8px', marginLeft: '34px' }}>
+                    <span className='font-medium text-text-secondary' style={{ fontSize: '14px', marginTop: '8px', marginLeft: '34px' }}>
                         Unlock your surprise now
                     </span>
 
@@ -146,26 +147,28 @@ const OnboardingBuild = ({ setCurrentStep }) => {
                         tag="button"
                         type="button"
                         variant="link"
-                        style={{ marginTop: '16px', marginRight: '168px', color: "#6005FF" }}
+                        style={{ marginTop: '16px', marginRight: '100px', color: "#6005FF" }}
                         onClick={() => setIsDialogOpen(true)}
                     >
                         Unlock My Surprise
                     </Button>
                 </div>
             </div>
-            <hr className="w-full border-b-0 border-x-0 border-t  border-solid border-t-border-subtle" style={{ marginTop: '44px', marginBottom: '44px' }} />
+            <hr className="w-full border-b-0 border-x-0 border-t  border-solid border-t-border-subtle" style={{ marginTop: '34px', marginBottom: '34px' }} />
 
-            <div className="bg-background-secondary border-[0.5px] border-subtle rounded-xl p-5 shadow-sm flex flex-col w-full space-y-1 space-x-2" style={{ width: '820px' }}>
+            <div className="bg-badge-background-gray border-[0.5px] border-subtle rounded-xl p-2 shadow-sm flex flex-col w-full space-y-1 space-x-2" style={{ width: '820px' }}>
                 <div className='flex flex-row items-center justify-start px-1 gap-3'>
                     <Switch
                         onChange=""
-                        size='md'
+                        size='sm'
                         value=""
                         className=""
                     />
-                    <Title size="xs" tag="h3" title={__('Help make UAE Better', 'header-footer-elementor')} />
+                     <p className="font-bold text-text-primary m-0" style={{ fontSize: '16px' }}>
+                        {__("Help make UAE Better", "header-footer-elementor")}
+                    </p>
                 </div>
-                <p className='text-base font-medium text-[#64748B]'>Help us improve by sharing anonymous data about your website setup. This includes non-sensitive info about plugins, themes, and settings, so we can create a better product for you. Your privacy is always our top priority. Learn more in our privacy policy.</p>
+                <p className='text-sm font-medium text-[#64748B]'>Help us improve by sharing anonymous data about your website setup. This includes non-sensitive info about plugins, themes, and settings, so we can create a better product for you. Your privacy is always our top priority. Learn more in our privacy policy.</p>
             </div>
 
             <Dialog
