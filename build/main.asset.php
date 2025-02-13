6fc9b5e2240eb33dac263e1811f94dfe6ddf5d78
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-02-13 10:46:39 +0530

    updated last success screen

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '91701bd7300f44686ebe');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0ff72d6bf70cc48a5457');
