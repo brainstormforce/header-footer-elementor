0c4545d7ef07120ea7d1d25df77eefda0dbb059a
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-02-14 12:44:10 +0530

    fixed switcher issue

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ec4a81ae3d14e856cc61');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1bb754a99a6c4b3d158d');
