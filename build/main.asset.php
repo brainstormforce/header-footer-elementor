2238a23a0fdb135854371ceed4f9fcd5eb520267
Sushma Kure <sushmak@bsf.io>
2025-02-15 13:39:48 +0530

    Fixed activate all/deactivate all buttons

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '039fb06f17de7dcd973b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7d0ff17d6dfe063732a1');
