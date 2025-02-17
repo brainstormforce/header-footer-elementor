dab70eea08bf74b3ac34959761f10b5b7680b859
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-02-17 15:01:28 +0530

    updated display rules for onboarding

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b70d9dfb86a7a06c78b6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'afa7a3dbd948ae265b1d');
