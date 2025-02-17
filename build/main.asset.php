c239376b9589e1e56be9b43b7ff642502470c764
Sushma Kure <sushmak@bsf.io>
2025-02-17 13:55:32 +0530

    Added conditions to Show onboarding only for new users only once.

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ffa6462c61b71234bc62');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9cea529aefab1a6202e1');
