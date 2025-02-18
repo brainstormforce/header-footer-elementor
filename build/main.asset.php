c02d1ec9262162c64d7be6e57a04ac334c54a859
Sushma Kure <sushmak@bsf.io>
2025-02-18 15:08:12 +0530

    Popup style fixes

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0a52bed8f960d15ff7a7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f8b678f43798bc0fa04f');
