0324912f841794c734dfcae2f2c66fdb23d79a68
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-02-18 10:57:12 +0530

    updated configure screen

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'adf51658f452be424211');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2138e820cdf17a73aade');
