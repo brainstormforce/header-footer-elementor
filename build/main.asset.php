422f8912844bfbb3efcee4f6ca7854d0977c2f02
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-02-18 10:38:02 +0530

    updated merge conflicts

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '504700ffded7280cc922');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'adf51658f452be424211');
