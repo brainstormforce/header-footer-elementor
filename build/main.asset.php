a74ec14f5d50d0881f6edb7ee5ced0d2ac942b39
Sushma Kure <sushmak@bsf.io>
2025-02-18 16:35:06 +0530

    Fixed styling issues

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9e0e522d832e4a745fbe');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd75a41581e2d24edf8f4');
