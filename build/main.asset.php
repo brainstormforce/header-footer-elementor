c21857bf8f82a43b28af748c7ff24019a394bfee
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-02-18 14:08:05 +0530

    updated styles for success screens

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3c3973ed930048889b8e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4ebd5e5262169a7cdfce');
