4c7fe29267be76de4508d4d8efb9a5be6fed230e
Sushma Kure <sushmak@bsf.io>
2025-02-19 11:32:59 +0530

    Updated spacing and scrollbar design

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4a520931c26e04bb2419');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '86e86dd75e3f24b4ed08');
