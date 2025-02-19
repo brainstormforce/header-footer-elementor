4d4b221d43af5a6c0d122cb49cd0d5414a96a613
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-02-19 18:25:30 +0530

    updated loader styles for configure screen

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'dcfee58736eaa4f604bb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6df13d4d4dcea9a9d10e');
