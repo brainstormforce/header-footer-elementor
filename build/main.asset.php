09074353cb214729005e8503914e19bc3ee313e0
Sushma Kure <sushmak@bsf.io>
2025-02-25 15:18:11 +0530

    Reverted package.json changes

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8ea3903d61553d574104');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'regenerator-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'db51784451a9ce191a11');
