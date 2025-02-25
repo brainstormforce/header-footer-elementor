f934431cbc7d90588c2feecdc3a5019ccf7424ba
Shubham Gupta <shubhamgmath@gmail.com>
2025-02-25 12:04:58 +0530

    Downgrade the forceUI library to 1.0.0

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '043fb21327abc6fb403a');
+<?php return array('dependencies' => array('react', 'react-dom', 'regenerator-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '09c8bf5effba25da8144');
