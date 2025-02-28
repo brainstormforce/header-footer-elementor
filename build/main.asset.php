f8928d3f8a5f497387247b26bae7ba15f8dbc583
Sushma Kure <sushmak@bsf.io>
2025-02-28 12:00:18 +0530

    Fix: pipeline issue

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8ea3903d61553d574104');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '78ef91a4006c2bfcc3fd');
