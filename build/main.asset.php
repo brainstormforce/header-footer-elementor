0bb4fa64ba9c5cf4cfa48cd9a51faf9aca3d4655
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-03-04 15:21:15 +0530

    added scrollable content to free vs pro page

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '03d99ce0f38ffa535a19');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f962a012e75d35cb3c3a');
