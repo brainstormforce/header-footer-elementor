921e62da1ee4e588f25f076509f3fcc716933a85
Shubham Gupta <shubhamgmath@gmail.com>
2025-03-12 16:33:26 +0530

    Updated dependency and run build

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1c961fb6b8b00e0ee148');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9105a4d56e81a9b811ed');
