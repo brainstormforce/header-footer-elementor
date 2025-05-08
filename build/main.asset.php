0005a0617939bc3c3f4fe4c4657485df4e24649b
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-05-08 11:14:58 +0530

    updated forceui library to 1.7.1

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'df784afd82b757ad41c0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2f99f4ef36d05abaddae');
