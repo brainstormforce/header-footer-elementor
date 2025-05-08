6775cf9a9fae3a8ae0e393d8ed42ea57d207047c
Sushma Kure <sushmak@bsf.io>
2025-05-08 11:33:56 +0530

    New: Added coupon section from onboarding

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'cf97fb2464d932854e53');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '49ae160fd89ae6ccbd13');
