9556d8e425a3cb07426d3d1260ffdf5c6357c3e5
Sushma Kure <sushmak@bsf.io>
2025-05-09 15:54:27 +0530

    Removed 2 days notice for coupon

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '719450863a182b65ab9f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '397a27a2a0d8d7ffba9f');
--- src/Components/Onboarding/Success.jsx
+++ src/Components/Onboarding/Success.jsx
@@ -160,13 +160,6 @@ const Success = () => {
                                             "header-footer-elementor"
                                         )}
                                     </span>
-                                    <p className="text-md font-medium italic text-text-primary m-0 mt-4" style={{ fontSize: '14px' }}>
-                                        <span style={{ color: 'red' }}>*</span>
-                                        {__(
-                                            " Use your exclusive discount code within the next 2 days to claim your reward!”",
-                                            "header-footer-elementor"
-                                        )}
-                                    </p>
                                 </div>
 
                                 <hr className="w-full border-b-0 border-x-0 border-t  border-solid border-t-border-subtle" style={{ marginTop: '2rem' }} />
