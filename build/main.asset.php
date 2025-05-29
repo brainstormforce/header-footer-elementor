177616734459428467ce10fdbb3e7eff7a98537f
Shubham Gupta <shubhamgmath@gmail.com>
2025-05-29 15:06:45 +0530

    removed unused code

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '983bfc908d4990100ef7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0fa3a1c419edee43c011');
