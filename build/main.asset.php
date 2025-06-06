c752d46aee01ebc0c4baee8b083cfeab7fc8c4cd
Shubham Gupta <shubhamgmath@gmail.com>
2025-06-06 12:59:40 +0530

    Added functionlity for deactivating unused widgets.

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0fa3a1c419edee43c011');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4a02e2fd5b1c69851fa5');
