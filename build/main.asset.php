fca5db967f143cf3e08d7d3623c8c237748f99f0
Shubham Gupta <shubhamgmath@gmail.com>
2025-06-12 20:21:31 +0530

    Updated Analytics setting content

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0fa3a1c419edee43c011');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '34308254cf6824a3486b');
