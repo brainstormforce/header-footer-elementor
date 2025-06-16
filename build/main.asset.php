65f86aebc4e3757d0218bfd356290f24650549fc
Shubham Gupta <shubhamgmath@gmail.com>
2025-06-16 08:15:45 +0530

    Updated build files

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8baa5ea1ac6992e21cf2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8d4d7681704d8bb20fd3');
