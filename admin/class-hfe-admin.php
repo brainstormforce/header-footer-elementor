03a8e37660519c78e853b51923cede9f4fb004e8
Shubham Gupta <shubhamgmath@gmail.com>
2025-06-24 07:05:49 +0530

    Revert "Fix custom button insertion timing"
    This reverts commit b47b928da6dceb8ec690b79031e27e8eadd9ebcc.

--- admin/class-hfe-admin.php
+++ admin/class-hfe-admin.php
@@ -131,7 +131,7 @@ class HFE_Admin {
                wp_enqueue_script(
                        'uae-pro-promotion',
                        HFE_URL . 'build/promotion-widget.js',
-                       [ 'jquery' ],
+                       [ 'jquery', 'wp-element', 'wp-dom-ready' ],
                        HFE_VER,
                        true
                );
