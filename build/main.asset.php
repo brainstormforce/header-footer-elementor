26768a37fc0ecca0aaf5851cfdde0a132ec9c958
Shubham Gupta <shubhamgmath@gmail.com>
2025-06-24 18:13:13 +0530

    Updated button width so it won't break on different screens

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1cb38c0f209df87d3987');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7e03dcf147ccad219f22');
