b15aae5b4be146dbe8f65614975250e4b030cbf5
Shubham Gupta <shubhamgmath@gmail.com>
2025-06-25 11:52:01 +0530

    Added tooltip for deactivate unused and revert deactivate all functionality

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7e03dcf147ccad219f22');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'cecd957563bd7cb6d582');
