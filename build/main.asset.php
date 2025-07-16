446d7c9264acb2d468f3203ae914dd60e758fd14
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-07-16 14:06:58 +0530

    updated ultimate features section

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6a6c8f28663751ca6a0b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '45a77bd5dd477608be06');
