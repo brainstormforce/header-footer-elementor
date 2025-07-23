42bafa05eccb35dd730e73b84b48fc10cd65e65a
Shubham Gupta <shubhamgmath@gmail.com>
2025-07-23 12:23:29 +0530

    Added utm parameters to dashboard tooltip  upgrade link

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '627c534f08b774a13ffb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '90d9ac96ebcb5d317537');
