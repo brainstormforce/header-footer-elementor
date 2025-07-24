279ad6a8ec6bd451aaa3e84b1d963e670e9d59dd
Shubham Gupta <shubhamgmath@gmail.com>
2025-07-24 13:58:20 +0530

    updated message for no bloat

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3f8446d2eaadc19df764');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9c9e6911cc26f40e4dcd');
