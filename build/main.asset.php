5866ccc5652dae852df68037798e2caacca20a55
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-07-29 14:59:37 +0530

    fixed accesssiblity for navmenu when using expanded layout

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '183a118202c0f7a34325');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ffb338a496be4d476435');
