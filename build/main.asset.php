a6c09003c004aa61ad9f3d086b8747ff9b523f63
Shubham Gupta <shubhamgmath@gmail.com>
2025-07-31 19:42:52 +0530

    Footer single screen functionality

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '657af50b9602723b6b20');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5b71973313f7fb50b201');
