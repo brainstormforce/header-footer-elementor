32d033257a93e8ba972a5db58a3c9226368d9c62
Shubham Gupta <shubhamgmath@gmail.com>
2025-08-01 15:48:11 +0530

    Updated code to fix display conditon modal working on new header create

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2cd1577b7aa23e5d85f6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '02d1ca650e31d2836671');
