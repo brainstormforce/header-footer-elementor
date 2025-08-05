f491824a05f5652c009bae5b5cb4fc71aaec3043
Shubham Gupta <shubhamgmath@gmail.com>
2025-08-05 13:57:44 +0530

    added gap-0 class to dialog

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '25f3ca0ca26bcb89080e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0b26c08010f161c2547e');
