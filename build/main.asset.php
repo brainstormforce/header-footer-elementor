4557cf578f52d2867141b84f53e7d54caf2c749b
Shubham Gupta <shubhamgmath@gmail.com>
2025-08-05 11:13:01 +0530

    Fixed dialog component rendering issue with create layout

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '77ef3d18aa04c8729310');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0e3caf795fdb5b85adea');
