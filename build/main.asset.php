05717403a0178f215ef3e0bb90d096596251b04f
Shubham Gupta <shubhamgmath@gmail.com>
2025-08-06 19:27:57 +0530

    Updated text size and  link for HF in nav menu

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '33b83c1f4f55cb9a2e60');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '821156dc6926437bb63a');
