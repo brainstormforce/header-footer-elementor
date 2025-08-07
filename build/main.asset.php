81646df28056a65e9be70068c9ff1dba6e550efa
Akshay Urankar <akshayurankar48@gmail.com>
2025-08-07 12:40:38 +0530

    updated texts and cta's for marketing and added topbar

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '183a118202c0f7a34325');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2e9cc511a7ce729f2aa5');
