100b3557dc9ee8098817837a6e6bbd774ec4b265
Akshay Urankar <akshayurankar48@gmail.com>
2025-08-07 10:28:31 +0530

    updated build files

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c21e454b14d8a97178a7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4417ea9914e47456d734');
