eb973d341f7ed8b63658147ab9eb6a47abf3adea
Akshay Urankar <akshayurankar48@gmail.com>
2025-08-07 07:21:10 +0530

    updated sticky position for sidebar CTA

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2fdb1ac27b42577bb4f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1724c3fad1ecd84a5dd3');
