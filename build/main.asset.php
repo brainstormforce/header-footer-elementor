3fa66aac5b657fabff3e84cc656a80217a36475e
Shubham Gupta <shubhamgmath@gmail.com>
2025-08-08 13:16:54 +0530

    FXIED Custom block changes edit with elementor

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '20c42d86d97423437b00');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8d75b69ab69cdfddfa74');
