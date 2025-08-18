1837dbaf3d81e35872ec28f585e215440fd1ef52
Akshay Urankar <akshayurankar48@gmail.com>
2025-08-18 12:35:01 +0530

    updated bounce issue on HFB

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '15ab09020b429f3b7a3d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd64828a9afc7cde34e00');
--- src/Components/HF-Builder/Navbar.js
+++ src/Components/HF-Builder/Navbar.js
@@ -14,6 +14,7 @@ import {
 import { __ } from "@wordpress/i18n";
 import { routes } from "../../admin/settings/routes";
 import { Link } from "../../router/index";
+import useWhatsNewRSS from "whats-new-rss";
 
 function updateNavMenuActiveState() {
 	const currentPath = window.location.hash;
@@ -53,11 +54,12 @@ const NavBar = () => {
 	// Get the current URL's hash part (after the #).
 	const currentPath = window.location.hash;
 
-	const isActive = (path) => currentPath.includes(path);
+	// Since this is the only setting on the navbar, always show it as active
+	const isActive = (path) => true; // Always return true to show as active
 
 	const linkStyle = (path) => ({
-		color: isActive(path) ? "#111827" : "#4B5563",
-		borderBottom: isActive(path) ? "2px solid #6005FF" : "none",
+		color: "#111827", // Always use active color
+		borderBottom: "2px solid #6005FF", // Always show active border
 		paddingBottom: "22px",
 		marginBottom: "-16px",
 	});
@@ -66,6 +68,32 @@ const NavBar = () => {
 		window.open(url, "_blank");
 		setIsDropdownOpen(false);
 	};
+
+	useWhatsNewRSS({
+		rssFeedURL: "https://ultimateelementor.com/whats-new/feed/",
+		selector: "#hfe-whats-new",
+		triggerButton: {
+			beforeBtn:
+				'<div class="w-4 sm:w-8 h-8 sm:h-10 flex items-center whitespace-nowrap justify-center cursor-pointer rounded-full border border-slate-200">',
+			icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#434141" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-megaphone"><path d="m3 11 18-5v12L3 14v-3z"></path><path d="M11.6 16.8a3 3 0 1 1-5.8-1.6"></path></svg>',
+			afterBtn: "</div>",
+		},
+		flyout: {
+			title: __("What's New?", "astra-sites"),
+			formatDate: (date) => {
+				const dayOfWeek = date.toLocaleDateString("en-US", {
+					weekday: "long",
+				});
+				const month = date.toLocaleDateString("en-US", {
+					month: "long",
+				});
+				const day = date.getDate();
+				const year = date.getFullYear();
+
+				return `${dayOfWeek} ${month} ${day}, ${year}`;
+			},
+		},
+	});
 
 	return (
 		<Topbar
@@ -94,7 +122,7 @@ const NavBar = () => {
 					</Topbar.Left>
 					<Topbar.Middle className="flex-grow" align="left">
 						<Topbar.Item>
-							<nav className="flex text-text-tertiary flex-wrap gap-2 mt-2 md:mt-0 cursor-pointer">
+							<nav className="flex flex-wrap gap-6 mt-2 md:mt-0 cursor-pointer">
 								<Link
 									to={routes.dashboard.path}
 									className={`${
@@ -129,107 +157,129 @@ const NavBar = () => {
 								</Link>
 							</nav>
 						</Topbar.Item>
-					</Topbar.Middle>
-					<Topbar.Right className="mr-6">
-						<Link to={routes.headerFooterBuilder.path}>
-							<div 
-								className="flex cursor-pointer mr-6 gap-2 items-center justify-center"
+						<Topbar.Item>
+							<Button
+								icon={<ArrowUpRight />}
+								iconPosition="right"
+								variant="ghost"
+								className="hfe-remove-ring mb-2"
 								style={{
-									backgroundColor: "#ffffff",
-									fontSize: "14px",
-									fontWeight: "400",
-									padding: "8px 12px",
-									borderRadius: "6px",
-									transition: "all 0.2s ease",
-									outline: "none",
-									transform: "scale(0.95)",
-									opacity: "1",
-									color: "#000000",
-									// border: "1px solid #e5e7eb",
+									color: "#6005FF",
+									background: "none",
+									border: "none",
+									padding: 0,
 									cursor: "pointer",
-									display: "inline-flex",
-									alignItems: "center",
-									justifyContent: "center",
-									gap: "8px",
-									boxShadow: "none",
 								}}
-								onMouseEnter={(e) => {
-									e.currentTarget.style.backgroundColor = "#ffffff";
-									e.currentTarget.style.color = "#000000";
-									// e.currentTarget.style.borderColor = "#d1d5db";
-									e.currentTarget.style.outline = "none";
-									e.currentTarget.style.boxShadow = "none";
-									e.currentTarget.style.transform = "scale(1)";
-								}}
-								onMouseLeave={(e) => {
-									e.currentTarget.style.backgroundColor = "#ffffff";
-									e.currentTarget.style.color = "#000000";
-									// e.currentTarget.style.borderColor = "#e5e7eb";
-									e.currentTarget.style.outline = "none";
-									e.currentTarget.style.boxShadow = "none";
-									e.currentTarget.style.transform = "scale(0.95)";
-								}}
+								onClick={() =>
+									handleRedirect(
+										"https://ultimateelementor.com/pricing/?utm_source=uae-lite-dashboard&utm_medium=navigation-bar&utm_campaign=uae-lite-upgrade"
+									)
+								}
 							>
-								<span className="text-black text-sm">
-									Switch to Table View
-								</span>
-								<AlignJustify 
-									className="cursor-pointer hfe-user-icon"
-									style={{ color: "black" }}
-									size={16}
-								/>
-							</div>
-						</Link>
-						<Link to={routes.dashboard.path}>
-							<div 
-								className="flex cursor-pointer items-center justify-center gap-1" 
-								style={{ 
-									paddingLeft: "0.5rem",
-									backgroundColor: "#ffffff",
-									fontSize: "14px",
-									fontWeight: "400",
-									padding: "8px 12px",
-									borderRadius: "6px",
-									transition: "all 0.2s ease",
-									outline: "none",
-									transform: "scale(0.95)",
-									opacity: "1",
-									color: "#000000",
-									// border: "1px solid #e5e7eb",
-									cursor: "pointer",
-									display: "inline-flex",
-									alignItems: "center",
-									justifyContent: "center",
-									gap: "8px",
-									boxShadow: "none",
-								}}
-								onMouseEnter={(e) => {
-									e.currentTarget.style.backgroundColor = "#ffffff";
-									e.currentTarget.style.color = "#000000";
-									// e.currentTarget.style.borderColor = "#d1d5db";
-									e.currentTarget.style.outline = "none";
-									e.currentTarget.style.boxShadow = "none";
-									e.currentTarget.style.transform = "scale(1)";
-								}}
-								onMouseLeave={(e) => {
-									e.currentTarget.style.backgroundColor = "#ffffff";
-									e.currentTarget.style.color = "#000000";
-									// e.currentTarget.style.borderColor = "#e5e7eb";
-									e.currentTarget.style.outline = "none";
-									e.currentTarget.style.boxShadow = "none";
-									e.currentTarget.style.transform = "scale(0.95)";
-								}}
-							>
-								<span className="text-black text-sm">
-									Close
-								</span>
-								<X
-									className="cursor-pointer hfe-user-icon"
-									style={{ color: "black" }}
-									size={16}
-								/>
-							</div>
-						</Link>
+								{__("Get Pro", "header-footer-elementor")}
+							</Button>
+						</Topbar.Item>
+					</Topbar.Middle>
+					<Topbar.Right className="gap-4">
+						<Topbar.Item>
+							<Link to={routes.headerFooterBuilder.path}>
+								<div 
+									className="flex cursor-pointer gap-2 items-center justify-center"
+									style={{
+										backgroundColor: "#ffffff",
+										fontSize: "14px",
+										fontWeight: "400",
+										padding: "8px 12px",
+										borderRadius: "6px",
+										transition: "all 0.2s ease",
+										outline: "none",
+										transform: "scale(0.95)",
+										opacity: "1",
+										color: "#000000",
+										cursor: "pointer",
+										display: "inline-flex",
+										alignItems: "center",
+										justifyContent: "center",
+										gap: "8px",
+										boxShadow: "none",
+									}}
+									onMouseEnter={(e) => {
+										e.currentTarget.style.backgroundColor = "#ffffff";
+										e.currentTarget.style.color = "#000000";
+										e.currentTarget.style.outline = "none";
+										e.currentTarget.style.boxShadow = "none";
+										e.currentTarget.style.transform = "scale(1)";
+									}}
+									onMouseLeave={(e) => {
+										e.currentTarget.style.backgroundColor = "#ffffff";
+										e.currentTarget.style.color = "#000000";
+										e.currentTarget.style.outline = "none";
+										e.currentTarget.style.boxShadow = "none";
+										e.currentTarget.style.transform = "scale(0.95)";
+									}}
+								>
+									<span className="text-black text-sm">
+										Switch to Table View
+									</span>
+									<AlignJustify 
+										className="cursor-pointer hfe-user-icon"
+										style={{ color: "black" }}
+										size={16}
+									/>
+								</div>
+							</Link>
+						</Topbar.Item>
+						<Topbar.Item>
+							<Link to={routes.dashboard.path}>
+								<div 
+									className="flex cursor-pointer items-center justify-center gap-1" 
+									style={{ 
+										backgroundColor: "#ffffff",
+										fontSize: "14px",
+										fontWeight: "400",
+										padding: "8px 12px",
+										borderRadius: "6px",
+										transition: "all 0.2s ease",
+										outline: "none",
+										transform: "scale(0.95)",
+										opacity: "1",
+										color: "#000000",
+										cursor: "pointer",
+										display: "inline-flex",
+										alignItems: "center",
+										justifyContent: "center",
+										gap: "8px",
+										boxShadow: "none",
+									}}
+									onMouseEnter={(e) => {
+										e.currentTarget.style.backgroundColor = "#ffffff";
+										e.currentTarget.style.color = "#000000";
+										e.currentTarget.style.outline = "none";
+										e.currentTarget.style.boxShadow = "none";
+										e.currentTarget.style.transform = "scale(1)";
+									}}
+									onMouseLeave={(e) => {
+										e.currentTarget.style.backgroundColor = "#ffffff";
+										e.currentTarget.style.color = "#000000";
+										e.currentTarget.style.outline = "none";
+										e.currentTarget.style.boxShadow = "none";
+										e.currentTarget.style.transform = "scale(0.95)";
+									}}
+								>
+									<span className="text-black text-sm">
+										Close
+									</span>
+									<X
+										className="cursor-pointer hfe-user-icon"
+										style={{ color: "black" }}
+										size={16}
+									/>
+								</div>
+							</Link>
+						</Topbar.Item>
+						<Topbar.Item className="gap-4 cursor-pointer">
+							<div className="pb-1" id="hfe-whats-new"></div>
+						</Topbar.Item>
 					</Topbar.Right>
 				</div>
 			</div>
