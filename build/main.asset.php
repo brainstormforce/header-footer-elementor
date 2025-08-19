5d0816c1488c8d272e3027f5220bf1931aaaafe7
Shubham Gupta <shubhamgmath@gmail.com>
2025-08-19 12:12:50 +0530

    Added dot

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd1e508f311bb73b69688');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '11812a261d05ce94f338');
