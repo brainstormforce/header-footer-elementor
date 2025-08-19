7ff0d0e36b73d3ca7d78416d5ab8678f320ccbc8
Shubham Gupta <shubhamgmath@gmail.com>
2025-08-19 14:06:05 +0530

    remove console log

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '11812a261d05ce94f338');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6ee7d2b28e57cc79c609');
