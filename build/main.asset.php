e9882ae28f9a997853908214f03adc6fcb550b93
Akshay Urankar <akshayurankar48@gmail.com>
2025-10-14 12:30:50 +0530

    updated styles for license CTA

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '571fcf70788c7137eba9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0ca389638a79117da090');
